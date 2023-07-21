7f937077cf02f007daf733c423dd955a8d5e0915
Jörg Thalheim <joerg@thalheim.io>
2023-07-21 13:30:35 +0200

    clan-cli/tests: fixup imports

--- pkgs/clan-cli/tests/test_clan_admin.py
+++ pkgs/clan-cli/tests/test_clan_admin.py
@@ -1,11 +1,11 @@
 import argparse
 
-import clan_admin
+from clan_cli import admin
 
 
 def test_make_parser():
     parser = argparse.ArgumentParser()
-    clan_admin.make_parser(parser)
+    admin.register_parser(parser)
 
 
 # using fp fixture from pytest-subprocess
@@ -13,5 +13,5 @@ def test_create(fp):
     cmd = ["nix", "flake", "init", "-t", fp.any()]
     fp.register(cmd)
     args = argparse.Namespace(folder="./my-clan")
-    clan_admin.create(args)
+    admin.create(args)
     assert fp.call_count(cmd) == 1
