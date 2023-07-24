930f1660e978997d2340a51d824f255ddbae7796
lassulus <git@lassul.us>
2023-07-24 11:31:52 +0200

    installer: wait for onion hostname to be available

--- installer.nix
+++ installer.nix
@@ -7,18 +7,21 @@
   ];
   services.openssh.settings.PermitRootLogin = "yes";
   system.activationScripts.root-password = ''
+    mkdir -p /var/shared
     ${pkgs.pwgen}/bin/pwgen -s 16 1 > /var/shared/root-password
     echo "root:$(cat /var/shared/root-password)" | chpasswd
   '';
   hidden-announce = {
     enable = true;
     script = pkgs.writers.writeDash "write-hostname" ''
+      mkdir -p /var/shared
       echo "$1" > /var/shared/onion-hostname
     '';
   };
   services.getty.autologinUser = lib.mkForce "root";
   programs.bash.interactiveShellInit = ''
     if [ "$(tty)" = "/dev/tty1" ]; then
+      until test -e /var/shared/onion-hostname; do sleep 1; done
       echo "ssh://root:$(cat /var/shared/root-password)@$(cat /var/shared/onion-hostname)"
     fi
   '';
