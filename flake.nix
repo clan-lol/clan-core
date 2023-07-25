076350efd42aabf1c20435d41a4c3528c2e5673e
Jörg Thalheim <joerg@thalheim.io>
2023-07-25 18:11:40 +0200

    disable darwin for now

--- flake.nix
+++ flake.nix
@@ -16,8 +16,6 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       imports = [
         ./flake-parts/packages.nix
