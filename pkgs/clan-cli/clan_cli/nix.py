72f6b368d6ef58dc332a9a73967d87989071cb9f
Jörg Thalheim <joerg@thalheim.io>
2023-07-26 08:17:37 +0200

    cli: resolve CLAN_NIXPKGS on every invocation
    this makes mocking easier

--- pkgs/clan-cli/clan_cli/nix.py
+++ pkgs/clan-cli/clan_cli/nix.py
@@ -1,10 +1,9 @@
 import os
 
-CLAN_NIXPKGS = os.environ.get("CLAN_NIXPKGS")
-
 
 def nix_shell(packages: list[str], cmd: list[str]) -> list[str]:
+    nixpkgs = os.environ.get("CLAN_NIXPKGS")
     # in unittest we will have all binaries provided
-    if CLAN_NIXPKGS is None:
+    if nixpkgs is None:
         return cmd
-    return ["nix", "shell", "-f", CLAN_NIXPKGS] + packages + ["-c"] + cmd
+    return ["nix", "shell", "-f", nixpkgs] + packages + ["-c"] + cmd
