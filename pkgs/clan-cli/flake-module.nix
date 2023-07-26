3d140394740f431284b7630b33ca6e59dd74d000
Jörg Thalheim <joerg@thalheim.io>
2023-07-26 08:29:34 +0200

    using flakes for devshell without reimporting nixpkgs multiple times

--- pkgs/clan-cli/flake-module.nix
+++ pkgs/clan-cli/flake-module.nix
@@ -4,9 +4,11 @@
       pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
       name = pyproject.project.name;
       package = pkgs.callPackage ./default.nix { };
+      shell = pkgs.callPackage ./shell.nix { };
     in
     {
       packages.${name} = package;
+      devShells.${name} = shell;
       packages.default = package;
       checks = package.tests;
     };
