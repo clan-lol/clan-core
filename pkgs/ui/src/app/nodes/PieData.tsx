b9c6ffd95ce7b7f56fdc287264e7ae6d7e995d31
Luis-Hebendanz <luis.nixos@gmail.com>
2023-08-09 17:31:28 +0200

    UI: Added /nodes page

---
+++ pkgs/ui/src/app/nodes/PieData.tsx
@@ -0,0 +1,36 @@
+
+
+export default function PieData() {
+    return [
+        {
+          "id": "scala",
+          "label": "scala",
+          "value": 317,
+          "color": "hsl(3, 70%, 50%)"
+        },
+        {
+          "id": "rust",
+          "label": "rust",
+          "value": 489,
+          "color": "hsl(113, 70%, 50%)"
+        },
+        {
+          "id": "css",
+          "label": "css",
+          "value": 456,
+          "color": "hsl(17, 70%, 50%)"
+        },
+        {
+          "id": "elixir",
+          "label": "elixir",
+          "value": 343,
+          "color": "hsl(232, 70%, 50%)"
+        },
+        {
+          "id": "haskell",
+          "label": "haskell",
+          "value": 167,
+          "color": "hsl(292, 70%, 50%)"
+        }
+      ];
+}
---
+++ pkgs/ui/src/app/nodes/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { StrictMode } from "react";
+import NodeList from "./NodeList";
+
+import Box from "@mui/material/Box";
+
+
+export default function Page() {
+    return (
+        <Box>
+
+
+            <NodeList />
+        </Box>
+    );
+}
