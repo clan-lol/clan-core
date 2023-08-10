c9b77e592725413cff28f97d706105f56610915d
Jörg Thalheim <joerg@thalheim.io>
2023-08-10 15:36:19 +0200

    clan-cli/tests/clan_flake: add flake.nix

--- pkgs/clan-cli/tests/clan_flake.py
+++ pkgs/clan-cli/tests/clan_flake.py
@@ -10,6 +10,15 @@ def clan_flake(temporary_dir: Path, monkeypatch: pytest.MonkeyPatch) -> Iterator
     flake = temporary_dir / "clan-flake"
     flake.mkdir()
     (flake / ".clan-flake").touch()
+    (flake / "flake.nix").write_text(
+        """
+{
+    description = "A flake for testing clan";
+    inputs = {};
+    outputs = { self, nixpkgs }: {};
+}
+"""
+    )
     monkeypatch.chdir(flake)
     with mock_env(HOME=str(temporary_dir)):
         yield flake
