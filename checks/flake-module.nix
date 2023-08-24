45cc2a78b5081c1f6cc0026ba9e6a45f909edc87
Jörg Thalheim <joerg@thalheim.io>
2023-08-24 14:38:24 +0200

    flake: test all flake outputs

--- checks/flake-module.nix
+++ checks/flake-module.nix
@@ -1,5 +1,5 @@
 { self, ... }: {
-  perSystem = { pkgs, ... }: {
+  perSystem = { pkgs, lib, self', ... }: {
     checks =
       let
         nixosTestArgs = {
@@ -15,7 +15,12 @@
         schemaTests = pkgs.callPackages ./schemas.nix {
           inherit self;
         };
+
+        flakeOutputs = lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations
+          // lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages
+          // lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells
+          // lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) (self'.legacyPackages.homeConfigurations or { });
       in
-      nixosTests // schemaTests;
+      nixosTests // schemaTests // flakeOutputs;
   };
 }
