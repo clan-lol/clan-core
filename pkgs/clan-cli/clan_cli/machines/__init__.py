c5b16124efa4628abe5995fb11eca3372ec9e8a0
Jörg Thalheim <joerg@thalheim.io>
2023-08-24 16:58:22 +0200

    add machine subcommand

--- pkgs/clan-cli/clan_cli/machines/__init__.py
+++ pkgs/clan-cli/clan_cli/machines/__init__.py
@@ -1,6 +1,9 @@
 # !/usr/bin/env python3
 import argparse
 
+from .create import register_create_parser
+from .delete import register_delete_parser
+from .list import register_list_parser
 from .update import register_update_parser
 
 
@@ -13,5 +16,14 @@ def register_parser(parser: argparse.ArgumentParser) -> None:
         required=True,
     )
 
-    groups_parser = subparser.add_parser("update", help="Update a machine")
-    register_update_parser(groups_parser)
+    update_parser = subparser.add_parser("update", help="Update a machine")
+    register_update_parser(update_parser)
+
+    create_parser = subparser.add_parser("create", help="Create a machine")
+    register_create_parser(create_parser)
+
+    delete_parser = subparser.add_parser("delete", help="Delete a machine")
+    register_delete_parser(delete_parser)
+
+    list_parser = subparser.add_parser("list", help="List machines")
+    register_list_parser(list_parser)
--- pkgs/clan-cli/clan_cli/secrets/types.py
+++ pkgs/clan-cli/clan_cli/secrets/types.py
@@ -9,31 +9,12 @@ from .sops import get_public_key
 
 VALID_SECRET_NAME = re.compile(r"^[a-zA-Z0-9._-]+$")
 VALID_USER_NAME = re.compile(r"^[a-z_]([a-z0-9_-]{0,31})?$")
-VALID_HOSTNAME = re.compile(r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?$", re.IGNORECASE)
-
-
-def validate_hostname(hostname: str) -> bool:
-    if len(hostname) > 63:
-        return False
-    return VALID_HOSTNAME.match(hostname) is not None
 
 
 def secret_name_type(arg_value: str) -> str:
     if not VALID_SECRET_NAME.match(arg_value):
         raise argparse.ArgumentTypeError(
             "Invalid character in secret name. Allowed characters are a-z, A-Z, 0-9, ., -, and _"
-        )
-    return arg_value
-
-
-def machine_name_type(arg_value: str) -> str:
-    if len(arg_value) > 63:
-        raise argparse.ArgumentTypeError(
-            "Machine name must be less than 63 characters long"
-        )
-    if not VALID_SECRET_NAME.match(arg_value):
-        raise argparse.ArgumentTypeError(
-            "Invalid character in machine name. Allowed characters are a-z, 0-9, ., -, and _. Must not start with a number"
         )
     return arg_value
 
