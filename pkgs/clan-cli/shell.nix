a3b408d8c8e173ebc00176fd5b7fb0d26291676e
Jörg Thalheim <joerg@thalheim.io>
2023-08-26 09:25:14 +0200

    use .direnv for generated project files

--- pkgs/clan-cli/shell.nix
+++ pkgs/clan-cli/shell.nix
@@ -26,21 +26,20 @@ pkgs.mkShell {
   # evaluating the flake .#
   CLAN_OPTIONS_FILE = ./clan_cli/config/jsonschema/options.json;
   shellHook = ''
-    tmp_path=$(realpath ./.pythonenv)
+    tmp_path=$(realpath ./.direnv)
     repo_root=$(realpath .)
-    rm -rf $tmp_path
-    mkdir -p "$tmp_path/${pythonWithDeps.sitePackages}"
+    mkdir -p "$tmp_path/python/${pythonWithDeps.sitePackages}"
 
     ${pythonWithDeps.interpreter} -m pip install \
       --quiet \
       --disable-pip-version-check \
       --no-index \
       --no-build-isolation \
-      --prefix "$tmp_path" \
+      --prefix "$tmp_path/python" \
       --editable $repo_root
 
     export PATH="$tmp_path/bin:${checkScript}/bin:$PATH"
-    export PYTHONPATH="$repo_root:$tmp_path/${pythonWithDeps.sitePackages}:${pythonWithDeps}/${pythonWithDeps.sitePackages}"
+    export PYTHONPATH="$repo_root:$tmp_path//python/${pythonWithDeps.sitePackages}:${pythonWithDeps}/${pythonWithDeps.sitePackages}"
 
     export XDG_DATA_DIRS="$tmp_path/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
     export fish_complete_path="$tmp_path/share/fish/vendor_completions.d''${fish_complete_path:+:$fish_complete_path}"
