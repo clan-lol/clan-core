e5db26d4b624eb11352f855f7aab7e42e7526a6f
Jörg Thalheim <joerg@thalheim.io>
2023-08-30 13:28:21 +0200

    set default template

--- templates/flake-module.nix
+++ templates/flake-module.nix
@@ -1,8 +1,9 @@
-{
+{ self, ... }: {
   flake.templates = {
     new-clan = {
       description = "Initialize a new clan flake";
       path = ./new-clan;
     };
+    default = self.templates.new-clan;
   };
 }
