414033392e1e36922c4ae09e946c41dfadf89506
DavHau <d.hauer.it@gmail.com>
2023-09-02 16:12:37 +0200

    new-clan: update template and add test

--- templates/new-clan/flake.nix
+++ templates/new-clan/flake.nix
@@ -3,9 +3,21 @@
 
   inputs.clan-core.url = "git+https://git.clan.lol/clan/clan-core";
 
-  outputs = { clan-core, ... }: {
-    nixosConfigurations = clan-core.lib.buildClan {
-      directory = ./.;
+  outputs = { clan-core, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      # all machines managed by cLAN
+      nixosConfigurations = clan-core.lib.buildClan {
+        directory = ./.;
+      };
+      # add the cLAN cli tool to the dev shell
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          clan-core.packages.${system}.clan-cli
+        ];
+      };
     };
-  };
 }
