211ce3e8c57a9331a6c6ed8c7ba38f639155cdaf
Johannes Kirschbauer <hsjobeki@gmail.com>
2023-09-23 13:20:25 +0200

    inspect flake before configure VM

---
+++ pkgs/ui/src/components/join/log.tsx
@@ -0,0 +1,19 @@
+interface LogOptions {
+  lines: string[];
+  title?: string;
+}
+export const Log = (props: LogOptions) => {
+  const { lines, title } = props;
+  return (
+    <div className="max-h-[70vh] min-h-[9rem] w-full overflow-scroll bg-slate-800 p-4 text-white shadow-inner shadow-black">
+      <div className="mb-1 text-slate-400">{title}</div>
+      <pre className="max-w-[90vw] text-xs">
+        {lines.map((item, idx) => (
+          <code key={`${idx}`} className="mb-2 block break-words">
+            {item}
+          </code>
+        ))}
+      </pre>
+    </div>
+  );
+};
