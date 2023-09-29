34dec8d1948825920863917ef1584de704b9b662
lassulus <git@lassul.us>
2023-09-29 18:31:05 +0200

    tests: add test_secrets_password_store

---
+++ pkgs/clan-cli/tests/test_flake_with_core_and_pass/flake.nix
@@ -0,0 +1,37 @@
+{
+  # Use this path to our repo root e.g. for UI test
+  # inputs.clan-core.url = "../../../../.";
+
+  # this placeholder is replaced by the path to nixpkgs
+  inputs.clan-core.url = "__CLAN_CORE__";
+
+  outputs = { self, clan-core }:
+    let
+      clan = clan-core.lib.buildClan {
+        directory = self;
+        machines = {
+          vm1 = { lib, ... }: {
+            clan.networking.deploymentAddress = "__CLAN_DEPLOYMENT_ADDRESS__";
+            system.stateVersion = lib.version;
+            clanCore.secretStore = "password-store";
+            clanCore.secretsUploadDirectory = lib.mkForce "__CLAN_SOPS_KEY_DIR__/secrets";
+
+            clan.networking.zerotier.controller.enable = true;
+
+            systemd.services.shutdown-after-boot = {
+              enable = true;
+              wantedBy = [ "multi-user.target" ];
+              after = [ "multi-user.target" ];
+              script = ''
+                #!/usr/bin/env bash
+                shutdown -h now
+              '';
+            };
+          };
+        };
+      };
+    in
+    {
+      inherit (clan) nixosConfigurations clanInternals;
+    };
+}
