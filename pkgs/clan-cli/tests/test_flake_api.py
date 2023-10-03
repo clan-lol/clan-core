e0ef03fa03ba25f07d8daf67bb2ec7440086d9f1
Qubasa <luis.nixos@gmail.com>
2023-10-03 12:50:47 +0200

    API: Added test for inspect_flake

--- pkgs/clan-cli/tests/test_flake_api.py
+++ pkgs/clan-cli/tests/test_flake_api.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-
+import json
 import pytest
 from api import TestClient
 
 
 @pytest.mark.impure
-def test_inspect(api: TestClient, test_flake_with_core: Path) -> None:
+def test_inspect_attrs(api: TestClient, test_flake_with_core: Path) -> None:
     params = {"url": str(test_flake_with_core)}
     response = api.get(
         "/api/flake/attrs",
@@ -15,3 +15,24 @@ def test_inspect(api: TestClient, test_flake_with_core: Path) -> None:
     data = response.json()
     print("Data: ", data)
     assert data.get("flake_attrs") == ["vm1"]
+
+
+
+@pytest.mark.impure
+def test_inspect_flake(api: TestClient, test_flake_with_core: Path) -> None:
+    params = {"url": str(test_flake_with_core)}
+    response = api.get(
+        "/api/flake",
+        params=params,
+    )
+    assert response.status_code == 200, "Failed to inspect vm"
+    data = response.json()
+    print("Data: ", json.dumps(data, indent=2))
+    assert data.get("content") is not None
+    actions = data.get("actions")
+    assert actions is not None
+    assert len(actions) == 2
+    assert actions[0].get("id") == "vms/inspect"
+    assert actions[0].get("uri") == "api/vms/inspect"
+    assert actions[1].get("id") == "vms/create"
+    assert actions[1].get("uri") == "api/vms/create"
