97a20053e7cbe9e1332bcd1eccbc9aee97967d61
Qubasa <consulting@qube.email>
2023-10-27 19:26:51 +0200

    Updated to main branch

--- pkgs/ui/src/app/machines/page.tsx
+++ pkgs/ui/src/app/machines/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { NodeTable } from "@/components/table";
-import { StrictMode } from "react";
+
 
 export default function Page() {
   return (
-    <StrictMode>
-      <NodeTable />
-    </StrictMode>
+    <NodeTable />
   );
 }
