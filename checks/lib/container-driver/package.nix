527db2dbf004ee0a61f008e8299e308ccf4c2ad8
Jörg Thalheim <joerg@thalheim.io>
2023-11-03 14:01:43 +0100

    container-driver: fix eval

--- checks/lib/container-driver/package.nix
+++ checks/lib/container-driver/package.nix
@@ -1,8 +1,8 @@
-{ extraPythonPackages, buildPythonApplication, self, setuptools, util-linux, systemd }:
+{ extraPythonPackages, python3Packages, buildPythonApplication, setuptools, util-linux, systemd }:
 buildPythonApplication {
   pname = "test-driver";
   version = "0.0.1";
-  propagatedBuildInputs = [ util-linux systemd ] ++ extraPythonPackages self;
+  propagatedBuildInputs = [ util-linux systemd ] ++ extraPythonPackages python3Packages;
   nativeBuildInputs = [ setuptools ];
   format = "pyproject";
   src = ./.;
