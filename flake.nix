483fb2ad9e8939e775b1f00d68454f62cfe5953c
Jörg Thalheim <joerg@thalheim.io>
2023-11-03 13:50:05 +0100

    switch to deltachat branch

--- flake.nix
+++ flake.nix
@@ -5,7 +5,9 @@
   nixConfig.extra-trusted-public-keys = [ "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28=" ];
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    # https://github.com/NixOS/nixpkgs/pull/265024
+    nixpkgs.url = "github:Mic92/nixpkgs/deltachat";
     floco.url = "github:aakropotkin/floco";
     floco.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
