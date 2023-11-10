46191b9a9c8a8f381db1acf231287e4a2fa3e26b
Jörg Thalheim <joerg@thalheim.io>
2023-11-10 12:46:15 +0100

    drop unused ClanDataPath

--- pkgs/clan-cli/clan_cli/webui/api_inputs.py
+++ pkgs/clan-cli/clan_cli/webui/api_inputs.py
@@ -4,19 +4,11 @@ from typing import Any
 
 from pydantic import AnyUrl, BaseModel, Extra, validator
 
-from ..dirs import clan_data_dir, clan_flakes_dir
+from ..dirs import clan_flakes_dir
 from ..flakes.create import DEFAULT_URL
 from ..types import validate_path
 
 log = logging.getLogger(__name__)
-
-
-class ClanDataPath(BaseModel):
-    directory: Path
-
-    @validator("directory")
-    def check_directory(cls: Any, v: Path) -> Path:  # noqa
-        return validate_path(clan_data_dir(), v)
 
 
 class ClanFlakePath(BaseModel):
