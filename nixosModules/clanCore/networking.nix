6a35f6b26cddcab5cffe89f2b0ecbf8e3d12611b
Jörg Thalheim <joerg@thalheim.io>
2023-11-15 06:54:29 +0100

    explicitly set networkd as the default

--- nixosModules/clanCore/networking.nix
+++ nixosModules/clanCore/networking.nix
@@ -18,4 +18,17 @@
       default = "root@${config.networking.hostName}";
     };
   };
+  config = {
+    # conflicts with systemd-resolved
+    networking.useHostResolvConf = false;
+
+    # The notion of "online" is a broken concept
+    # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+    systemd.services.NetworkManager-wait-online.enable = false;
+    systemd.network.wait-online.enable = false;
+
+    # Use networkd instead of the pile of shell scripts
+    networking.useNetworkd = lib.mkDefault true;
+    networking.useDHCP = lib.mkDefault false;
+  };
 }
