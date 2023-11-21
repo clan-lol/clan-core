90ff10eac18fd681f6b5d1686e32e1d29bfd5cc9
Jörg Thalheim <joerg@thalheim.io>
2023-11-21 13:56:27 +0100

    Revert "nix-unit; update"
    This reverts commit bd7899e48ac7972309c8a591d420b98543504034.

--- pkgs/nix-unit/default.nix
+++ pkgs/nix-unit/default.nix
@@ -1,8 +1,43 @@
-{ callPackage }:
-let
-  nix-unit-src = builtins.fetchGit {
-    url = "https://github.com/adisbladis/nix-unit";
+{ stdenv
+, lib
+, nixVersions
+, fetchFromGitHub
+, nlohmann_json
+, boost
+, meson
+, pkg-config
+, ninja
+, cmake
+, clang-tools
+}:
+
+stdenv.mkDerivation {
+  pname = "nix-unit";
+  version = "0.1";
+  src = fetchFromGitHub {
+    owner = "adisbladis";
+    repo = "nix-unit";
     rev = "7e2ee1c70f930b9b65b9fc33c3f3eca0dfae00d1";
+    sha256 = "sha256-UaUkh+/lxzNCRH64YB6SbyRIvvDhgY98izX9CvWgJA4=";
   };
-in
-callPackage nix-unit-src { }
+  buildInputs = [
+    nlohmann_json
+    nixVersions.stable
+    boost
+  ];
+  nativeBuildInputs = [
+    meson
+    pkg-config
+    ninja
+    # nlohmann_json can be only discovered via cmake files
+    cmake
+  ] ++ (lib.optional stdenv.cc.isClang [ clang-tools ]);
+
+  meta = {
+    description = "Nix unit test runner";
+    homepage = "https://github.com/adisbladis/nix-unit";
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ adisbladis ];
+    platforms = lib.platforms.unix;
+  };
+}
