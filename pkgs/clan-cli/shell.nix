c00a1983fae74bf1e74872997f38e729b446333d
Qubasa <consulting@qube.email>
2023-12-09 00:23:19 +0100

    Removed democlan dependency

--- pkgs/clan-cli/shell.nix
+++ pkgs/clan-cli/shell.nix
@@ -1,4 +1,4 @@
-{ nix-unit, clan-cli, ui-assets, system, mkShell, writeScriptBin, openssh, ruff, python3, democlan ? null }:
+{ nix-unit, clan-cli, ui-assets, system, mkShell, writeScriptBin, openssh, ruff, python3 }:
 let
   checkScript = writeScriptBin "check" ''
     nix build .#checks.${system}.{treefmt,clan-pytest} -L "$@"
@@ -24,7 +24,6 @@ mkShell {
   shellHook = ''
     tmp_path=$(realpath ./.direnv)
 
-    # export DEMOCLAN_ROOT=${democlan}
     repo_root=$(realpath .)
     mkdir -p "$tmp_path/python/${pythonWithDeps.sitePackages}"
 
