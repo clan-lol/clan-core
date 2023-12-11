040e06bcdda1aa1b15f81b5e111d9f9cb9503a50
Qubasa <consulting@qube.email>
2023-12-11 19:26:29 +0100

    Improved history command

--- pkgs/clan-cli/clan_cli/history/update.py
+++ pkgs/clan-cli/clan_cli/history/update.py
@@ -11,19 +11,22 @@ from .add import EnhancedJSONEncoder, HistoryEntry, get_dir_time, list_history
 
 
 def update_history() -> list[HistoryEntry]:
+    logs = list_history()
+
+    new_logs = []
+    for entry in logs:
+        new_entry = copy.deepcopy(entry)
+        new_time = get_dir_time(Path(entry.path))
+        if new_time != entry.dir_datetime:
+            print(f"Updating {entry.path} from {entry.dir_datetime} to {new_time}")
+            new_entry.dir_datetime = new_time
+            new_entry.last_used = datetime.datetime.now().isoformat()
+        new_logs.append(new_entry)
+
     with locked_open(user_history_file(), "w+") as f:
-        logs = list_history()
-        new_logs = []
-        for entry in logs:
-            new_entry = copy.deepcopy(entry)
-            new_time = get_dir_time(Path(entry.path))
-            if new_time != entry.dir_datetime:
-                new_entry.dir_datetime = new_time
-                new_entry.last_used = datetime.datetime.now().isoformat()
-            new_logs.append(new_entry)
         f.write(json.dumps(new_logs, cls=EnhancedJSONEncoder, indent=4))
         f.truncate()
-        return new_logs
+    return new_logs
 
 
 def add_update_command(args: argparse.Namespace) -> None:
