a1ebe663ce409e513a170d697b5c94de92b28c42
Qubasa <consulting@qube.email>
2023-12-12 13:56:54 +0100

    Fixing pytest

---
+++ pkgs/clan-cli/tests/test_history_cli.py
@@ -0,0 +1,51 @@
+import json
+from typing import TYPE_CHECKING
+
+import pytest
+from cli import Cli
+from fixtures_flakes import FlakeForTest
+from pytest import CaptureFixture
+
+from clan_cli.dirs import user_history_file
+from clan_cli.history.add import HistoryEntry
+
+if TYPE_CHECKING:
+    pass
+
+
+def test_history_add(
+    test_flake: FlakeForTest,
+) -> None:
+    cli = Cli()
+    cmd = [
+        "history",
+        "add",
+        str(test_flake.path),
+    ]
+
+    cli.run(cmd)
+
+    history_file = user_history_file()
+    assert history_file.exists()
+    history = [HistoryEntry(**entry) for entry in json.loads(open(history_file).read())]
+    assert history[0].path == str(test_flake.path)
+
+
+def test_history_list(
+    capsys: CaptureFixture,
+    test_flake: FlakeForTest,
+) -> None:
+    cli = Cli()
+    cmd = [
+        "history",
+        "list",
+    ]
+
+    cli.run(cmd)
+    assert str(test_flake.path) not in capsys.readouterr().out
+
+    cli.run(["history", "add", str(test_flake.path)])
+    cli.run(cmd)
+    assert str(test_flake.path) in capsys.readouterr().out
+
+
