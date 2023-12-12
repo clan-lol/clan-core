0a9fb41785cef41f4a66b8cd230bc8fe19a4e6b9
Qubasa <consulting@qube.email>
2023-12-12 21:31:47 +0100

    Improved ClanURI

--- pkgs/clan-cli/tests/test_history_cli.py
+++ pkgs/clan-cli/tests/test_history_cli.py
@@ -13,26 +13,26 @@ if TYPE_CHECKING:
 
 
 def test_history_add(
-    test_flake: FlakeForTest,
+    test_flake_with_core: FlakeForTest,
 ) -> None:
     cli = Cli()
     cmd = [
         "history",
         "add",
-        str(test_flake.path),
+        str(test_flake_with_core.path),
     ]
-    breakpoint()
+
     cli.run(cmd)
 
     history_file = user_history_file()
     assert history_file.exists()
     history = [HistoryEntry(**entry) for entry in json.loads(open(history_file).read())]
-    assert history[0].flake.flake_url == str(test_flake.path)
+    assert history[0].flake.flake_url == str(test_flake_with_core.path)
 
 
 def test_history_list(
     capsys: CaptureFixture,
-    test_flake: FlakeForTest,
+    test_flake_with_core: FlakeForTest,
 ) -> None:
     cli = Cli()
     cmd = [
@@ -41,8 +41,8 @@ def test_history_list(
     ]
 
     cli.run(cmd)
-    assert str(test_flake.path) not in capsys.readouterr().out
+    assert str(test_flake_with_core.path) not in capsys.readouterr().out
 
-    cli.run(["history", "add", str(test_flake.path)])
+    cli.run(["history", "add", str(test_flake_with_core.path)])
     cli.run(cmd)
-    assert str(test_flake.path) in capsys.readouterr().out
+    assert str(test_flake_with_core.path) in capsys.readouterr().out
