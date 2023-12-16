cc3701f22fae6df2af8736f6eea5563d804c8f7d
lassulus <git@lassul.us>
2023-12-16 21:57:27 +0100

    checks backup: limit to linux systems

--- checks/backups/flake-module.nix
+++ checks/backups/flake-module.nix
@@ -64,62 +64,64 @@ in
       };
   };
   perSystem = { nodes, pkgs, ... }: {
-    checks.test-backups =
-      (import ../lib/test-base.nix)
-        {
-          name = "test-backups";
-          nodes.server = {
-            imports = [
-              self.nixosModules.test_backup_server
-              self.nixosModules.clanCore
-              {
-                clanCore.machineName = "server";
-                clanCore.clanDir = ../..;
-              }
-            ];
-          };
-          nodes.client = {
-            imports = [
-              self.nixosModules.test_backup_client
-              self.nixosModules.clanCore
-              {
-                clanCore.machineName = "client";
-                clanCore.clanDir = ../..;
-              }
-            ];
-          };
+    checks = pkgs.lib.mkIf (pkgs.stdenv.isLinux) {
+      test-backups =
+        (import ../lib/test-base.nix)
+          {
+            name = "test-backups";
+            nodes.server = {
+              imports = [
+                self.nixosModules.test_backup_server
+                self.nixosModules.clanCore
+                {
+                  clanCore.machineName = "server";
+                  clanCore.clanDir = ../..;
+                }
+              ];
+            };
+            nodes.client = {
+              imports = [
+                self.nixosModules.test_backup_client
+                self.nixosModules.clanCore
+                {
+                  clanCore.machineName = "client";
+                  clanCore.clanDir = ../..;
+                }
+              ];
+            };
 
-          testScript = ''
-            import json
-            start_all()
+            testScript = ''
+              import json
+              start_all()
 
-            # setup
-            client.succeed("mkdir -m 700 /root/.ssh")
-            client.succeed(
-                "cat ${../borgbackup/borg_test} > /root/.ssh/id_ed25519"
-            )
-            client.succeed("chmod 600 /root/.ssh/id_ed25519")
-            client.wait_for_unit("sshd", timeout=30)
-            print(client.succeed("ssh -o StrictHostKeyChecking=accept-new -v root@client hostname"))
+              # setup
+              client.succeed("mkdir -m 700 /root/.ssh")
+              client.succeed(
+                  "cat ${../borgbackup/borg_test} > /root/.ssh/id_ed25519"
+              )
+              client.succeed("chmod 600 /root/.ssh/id_ed25519")
+              client.wait_for_unit("sshd", timeout=30)
+              print(client.succeed("ssh -o StrictHostKeyChecking=accept-new -v root@client hostname"))
 
-            # dummy data
-            client.succeed("mkdir /var/test-backups")
-            client.succeed("echo testing > /var/test-backups/somefile")
+              # dummy data
+              client.succeed("mkdir /var/test-backups")
+              client.succeed("echo testing > /var/test-backups/somefile")
 
-            # create
-            client.succeed("clan --flake ${../..} backups create test_backup_client")
-            client.wait_until_succeeds("! systemctl is-active borgbackup-job-test_backup_server")
+              # create
+              client.succeed("clan --flake ${../..} backups create test_backup_client")
+              client.wait_until_succeeds("! systemctl is-active borgbackup-job-test_backup_server")
 
-            # list
-            backup_id = json.loads(client.succeed("borg-job-test_backup_server list --json"))["archives"][0]["archive"]
-            assert(backup_id in client.succeed("clan --flake ${../..} backups list test_backup_client"))
+              # list
+              backup_id = json.loads(client.succeed("borg-job-test_backup_server list --json"))["archives"][0]["archive"]
+              assert(backup_id in client.succeed("clan --flake ${../..} backups list test_backup_client"))
 
-            # restore
-            client.succeed("rm -f /var/test-backups/somefile")
-            client.succeed(f"clan --flake ${../..} backups restore test_backup_client borgbackup {backup_id}")
-            assert(client.succeed("cat /var/test-backups/somefile").strip() == "testing")
-          '';
-        }
-        { inherit pkgs self; };
+              # restore
+              client.succeed("rm -f /var/test-backups/somefile")
+              client.succeed(f"clan --flake ${../..} backups restore test_backup_client borgbackup {backup_id}")
+              assert(client.succeed("cat /var/test-backups/somefile").strip() == "testing")
+            '';
+          }
+          { inherit pkgs self; };
+    };
   };
 }
