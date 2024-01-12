c86e465e3506f0776a52b345d34f996d0356c61b
Jörg Thalheim <joerg@thalheim.io>
2024-01-12 11:58:16 +0100

    drop qemu-rutabaga patches

--- pkgs/qemu-wayland/default.nix
+++
@@ -1,9 +0,0 @@
-{ qemu_kvm, rutabaga-gfx-ffi, fetchurl }:
-qemu_kvm.overrideAttrs (old: {
-  src = fetchurl {
-    url = "https://download.qemu.org/qemu-8.2.0.tar.xz";
-    hash = "sha256-vwDS+hIBDfiwrekzcd71jmMssypr/cX1oP+Oah+xvzI=";
-  };
-
-  buildInputs = old.buildInputs ++ [ rutabaga-gfx-ffi ];
-})
