ed6eb50f7d2a9a319ae17dc8de575998885d4d1f
lassulus <git@lassul.us>
2024-01-15 19:34:04 +0100

    WIP: clan-cli secrets: add secret_store as python class

--- pkgs/clan-cli/clan_cli/secrets/generate.py
+++ pkgs/clan-cli/clan_cli/secrets/generate.py
@@ -1,27 +1,78 @@
 import argparse
 import logging
 import os
-import sys
+import shutil
+import types
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
-from clan_cli.cmd import Log, run
+from clan_cli.cmd import run
 
+from ..errors import ClanError
 from ..machines.machines import Machine
+from ..nix import nix_shell
 
 log = logging.getLogger(__name__)
 
 
 def generate_secrets(machine: Machine) -> None:
-    env = os.environ.copy()
-    env["CLAN_DIR"] = str(machine.flake_dir)
-    env["PYTHONPATH"] = ":".join(sys.path)  # TODO do this in the clanCore module
+    # load secrets module from file
+    loader = SourceFileLoader("secret_module", machine.secrets_module)
+    secrets_module = types.ModuleType(loader.name)
+    loader.exec_module(secrets_module)
+    secret_store = secrets_module.SecretStore(machine=machine)
 
-    print(f"generating secrets... {machine.generate_secrets}")
-    run(
-        [machine.generate_secrets],
-        env=env,
-        error_msg="failed to generate secrets",
-        log=Log.BOTH,
-    )
+    with TemporaryDirectory() as d:
+        for service in machine.secrets_data:
+            print(service)
+            tmpdir = Path(d) / service
+            # check if all secrets exist and generate them if at least one is missing
+            needs_regeneration = any(
+                not secret_store.exists(service, secret)
+                for secret in machine.secrets_data[service]["secrets"]
+            ) or any(
+                not (machine.flake_dir / fact).exists()
+                for fact in machine.secrets_data[service]["facts"].values()
+            )
+            for fact in machine.secrets_data[service]["facts"].values():
+                if not (machine.flake_dir / fact).exists():
+                    print(f"fact {fact} is missing")
+            if needs_regeneration:
+                env = os.environ.copy()
+                facts_dir = tmpdir / "facts"
+                facts_dir.mkdir(parents=True)
+                env["facts"] = str(facts_dir)
+                secrets_dir = tmpdir / "secrets"
+                secrets_dir.mkdir(parents=True)
+                env["secrets"] = str(secrets_dir)
+                # TODO use bubblewrap here
+                cmd = nix_shell(
+                    ["nixpkgs#bash"],
+                    ["bash", "-c", machine.secrets_data[service]["generator"]],
+                )
+                run(
+                    cmd,
+                    env=env,
+                )
+                # store secrets
+                for secret in machine.secrets_data[service]["secrets"]:
+                    secret_file = secrets_dir / secret
+                    if not secret_file.is_file():
+                        msg = f"did not generate a file for '{secret}' when running the following command:\n"
+                        msg += machine.secrets_data[service]["generator"]
+                        raise ClanError(msg)
+                    secret_store.set(service, secret, secret_file.read_text())
+                # store facts
+                for name, fact_path in machine.secrets_data[service]["facts"].items():
+                    fact_file = facts_dir / name
+                    if not fact_file.is_file():
+                        msg = f"did not generate a file for '{name}' when running the following command:\n"
+                        msg += machine.secrets_data[service]["generator"]
+                        raise ClanError(msg)
+                    fact_path = machine.flake_dir / fact_path
+                    fact_path.parent.mkdir(parents=True, exist_ok=True)
+                    shutil.copyfile(fact_file, fact_path)
 
     print("successfully generated secrets")
 
--- pkgs/clan-cli/clan_cli/secrets/upload.py
+++ pkgs/clan-cli/clan_cli/secrets/upload.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import types
+from importlib.machinery import SourceFileLoader
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -11,29 +13,41 @@ log = logging.getLogger(__name__)
 
 
 def upload_secrets(machine: Machine) -> None:
-    with TemporaryDirectory() as tempdir_:
-        tempdir = Path(tempdir_)
-        should_upload = machine.run_upload_secrets(tempdir)
+    # load secrets module from file
+    loader = SourceFileLoader("secret_module", machine.secrets_module)
+    secrets_module = types.ModuleType(loader.name)
+    loader.exec_module(secrets_module)
+    secret_store = secrets_module.SecretStore(machine=machine)
 
-        if should_upload:
-            host = machine.host
+    update_check = getattr(secret_store, "update_check", None)
+    if callable(update_check):
+        if update_check():
+            log.info("Secrets already up to date")
+            return
+    with TemporaryDirectory() as tempdir:
+        secrets = []
+        for service in machine.secrets_data:
+            for secret in machine.secrets_data[service]["secrets"]:
+                secrets.append((service, secret))
+        secret_store.upload(Path(tempdir), secrets)
+        host = machine.host
 
-            ssh_cmd = host.ssh_cmd()
-            run(
-                nix_shell(
-                    ["nixpkgs#rsync"],
-                    [
-                        "rsync",
-                        "-e",
-                        " ".join(["ssh"] + ssh_cmd[2:]),
-                        "-az",
-                        "--delete",
-                        f"{tempdir!s}/",
-                        f"{host.user}@{host.host}:{machine.secrets_upload_directory}/",
-                    ],
-                ),
-                log=Log.BOTH,
-            )
+        ssh_cmd = host.ssh_cmd()
+        run(
+            nix_shell(
+                ["nixpkgs#rsync"],
+                [
+                    "rsync",
+                    "-e",
+                    " ".join(["ssh"] + ssh_cmd[2:]),
+                    "-az",
+                    "--delete",
+                    f"{tempdir!s}/",
+                    f"{host.user}@{host.host}:{machine.secrets_upload_directory}/",
+                ],
+            ),
+            log=Log.BOTH,
+        )
 
 
 def upload_command(args: argparse.Namespace) -> None:
