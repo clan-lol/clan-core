d99ef89f7cb2a32ac727499327ab1bfe93845f05
Qubasa <consulting@qube.email>
2024-02-08 15:00:36 +0700

    UI: Fixed incorrect display of cLAN icon in window switcher

--- pkgs/clan-vm-manager/clan_vm_manager/constants.py
+++
@@ -1,7 +0,0 @@
-constants = {
-    "APPID": "clan.lol",
-    "APPVERSION": "2.0.0-beta",
-    "APPNAME": "clan-vm-manager",
-    "SYSNAME": "clan-manager",
-    "RESOURCEID": "/git.clan.lol/clan/clan-core",
-}
