b77ffac4d45403ef1b5fbeefacd2e1729183a821
Qubasa <consulting@qube.email>
2024-03-04 15:52:49 +0700

    clan_vm_manager: Cache profiler obj for multi call functions

--- pkgs/clan-vm-manager/clan_vm_manager/__init__.py
+++ pkgs/clan-vm-manager/clan_vm_manager/__init__.py
@@ -2,12 +2,10 @@ import logging
 import sys
 
 from clan_vm_manager.app import MainApplication
-from clan_vm_manager.components.profiler import profile
 
 log = logging.getLogger(__name__)
 
 
-@profile
 def main() -> int:
     app = MainApplication()
     return app.run(sys.argv)
