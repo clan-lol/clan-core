32d0f1ccd440f40dfd4f9c7c6347ea0e23c0a4c9
a-kenji <aks.kenji@protonmail.com>
2024-03-08 09:47:44 +0100

    init: sunshine-moonlight-accept module

--- clanModules/sunshine.nix
+++ clanModules/sunshine.nix
@@ -1,4 +1,16 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
+let
+  ms-accept = pkgs.callPackage ../pkgs/moonlight-sunshine-accept { };
+  sunshineConfiguration = pkgs.writeText "sunshine.conf" ''
+    address_family = both
+    channels = 5
+    pkey = /var/lib/sunshine/sunshine.key
+    cert = /var/lib/sunshine/sunshine.cert
+    file_state = /var/lib/sunshine/state.json
+    credentials_file = /var/lib/sunshine/credentials.json
+  '';
+  listenPort = 48011;
+in
 {
   networking.firewall = {
     allowedTCPPorts = [
@@ -6,6 +18,7 @@
       47989
       47990
       48010
+      48011
     ];
 
     allowedUDPPorts = [
@@ -29,17 +42,28 @@
       to = 48010;
     }
   ];
+  networking.firewall.interfaces."zt+".allowedTCPPorts = [
+    47984
+    47989
+    47990
+    48010
+    listenPort
+  ];
+  networking.firewall.interfaces."zt+".allowedUDPPortRanges = [
+    {
+      from = 47998;
+      to = 48010;
+    }
+  ];
+
 
   environment.systemPackages = [
+    ms-accept
     pkgs.sunshine
     pkgs.avahi
     # Convenience script, until we find a better UX
     (pkgs.writers.writeDashBin "sun" ''
-      ${pkgs.sunshine}/bin/sunshine -1 ${
-        pkgs.writeText "sunshine.conf" ''
-          address_family = both
-        ''
-      } "$@"
+      ${pkgs.sunshine}/bin/sunshine -0 ${sunshineConfiguration} "$@"
     '')
     # Create a dummy account, for easier setup,
     # don't use this account in actual production yet.
@@ -51,32 +75,118 @@
 
   # Required to simulate input
   boot.kernelModules = [ "uinput" ];
-  security.rtkit.enable = true;
-
-  # services.udev.extraRules = ''
-  #   KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  # '';
 
   services.udev.extraRules = ''
-    KERNEL=="uinput", GROUP="input", MODE="0660" OPTIONS+="static_node=uinput"
+    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
   '';
 
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
+  security = {
+    rtkit.enable = true;
+    wrappers.sunshine = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_sys_admin+p";
+      source = "${pkgs.sunshine}/bin/sunshine";
+    };
   };
 
+  systemd.tmpfiles.rules = [
+    "d '/var/lib/sunshine' 0770 'user' 'users' - -"
+    "C '/var/lib/sunshine/sunshine.cert' 0644 'user' 'users' - ${config.clanCore.secrets.sunshine.secrets."sunshine.cert".path or ""}"
+    "C '/var/lib/sunshine/sunshine.key' 0644 'user' 'users' - ${config.clanCore.secrets.sunshine.secrets."sunshine.key".path or ""}"
+  ];
+
+  hardware.opengl.enable = true;
+
   systemd.user.services.sunshine = {
-    description = "sunshine";
-    wantedBy = [ "graphical-session.target" ];
-    environment = {
-      DISPLAY = ":0";
-    };
+    enable = true;
+    description = "Sunshine self-hosted game stream host for Moonlight";
+    startLimitBurst = 5;
+    startLimitIntervalSec = 500;
+    script = "/run/current-system/sw/bin/env /run/wrappers/bin/sunshine ${sunshineConfiguration}";
     serviceConfig = {
-      ExecStart = "${config.security.wrapperDir}/sunshine";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      ReadWritePaths = [
+        "/var/lib/sunshine"
+      ];
+      ReadOnlyPaths = [
+        (config.clanCore.secrets.sunshine.secrets."sunshine.key".path or "")
+        (config.clanCore.secrets.sunshine.secrets."sunshine.cert".path or "")
+      ];
     };
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [
+      "sunshine-init-state.service"
+      "sunshine-init-credentials.service"
+    ];
+  };
+
+  systemd.user.services.sunshine-init-state = {
+    enable = true;
+    description = "Sunshine self-hosted game stream host for Moonlight";
+    startLimitBurst = 5;
+    startLimitIntervalSec = 500;
+    script = ''
+      ${ms-accept}/bin/moonlight-sunshine-accept sunshine init-state --uuid ${config.clanCore.secrets.sunshine.facts.sunshine-uuid.value or null} --state-file /var/lib/sunshine/state.json
+    '';
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "5s";
+      Type = "oneshot";
+      ReadWritePaths = [
+        "/var/lib/sunshine"
+      ];
+    };
+    wantedBy = [ "graphical-session.target" ];
+  };
+
+  systemd.user.services.sunshine-init-credentials = {
+    enable = true;
+    description = "Sunshine self-hosted game stream host for Moonlight";
+    startLimitBurst = 5;
+    startLimitIntervalSec = 500;
+    script = ''
+      ${lib.getExe pkgs.sunshine} --creds sunshine sunshine
+    '';
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "5s";
+      Type = "oneshot";
+      ReadWritePaths = [
+        "/var/lib/sunshine"
+      ];
+    };
+    wantedBy = [ "graphical-session.target" ];
+  };
+
+  systemd.user.services.sunshine-listener = {
+    enable = true;
+    description = "Sunshine self-hosted game stream host for Moonlight";
+    startLimitBurst = 5;
+    startLimitIntervalSec = 500;
+    script = ''
+      ${ms-accept}/bin/moonlight-sunshine-accept sunshine listen --port ${builtins.toString listenPort} --uuid ${config.clanCore.secrets.sunshine.facts.sunshine-uuid.value or null}  --state /var/lib/sunshine/state.json --cert '${config.clanCore.secrets.sunshine.facts."sunshine.cert".value or null}'
+    '';
+    serviceConfig = {
+      # ExecStart = lib.concatStringsSep " " (lib.flatten 
+      # [
+      # (lib.getExe ms-accept) "sunshine" "listen"
+      # "--port" (builtins.toString listenPort)
+      # "--uuid" (config.clanCore.secrets.sunshine.facts."sunshine-uuid".value or "")
+      # "--state" "/var/lib/sunshine/state.json"
+      # "--cert" (config.clanCore.secrets.sunshine.facts."sunshine.cert".value or "")
+      # ]
+      # );
+      Restart = "on-failure";
+      RestartSec = 5;
+      ReadWritePaths = [
+        "/var/lib/sunshine"
+      ];
+    };
+    wantedBy = [ "graphical-session.target" ];
   };
 
   # xdg.configFile."sunshine/apps.json".text = builtins.toJSON {
@@ -93,17 +203,21 @@
   #   ];
   # };
 
-  services = {
-    avahi = {
-      enable = true;
-      reflector = true;
-      nssmdns = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        userServices = true;
-        workstation = true;
-      };
-    };
+  clanCore.secrets.sunshine = {
+    secrets."sunshine.key" = { };
+    secrets."sunshine.cert" = { };
+    facts."sunshine-uuid" = { };
+    facts."sunshine.cert" = { };
+    generator.path = [
+      pkgs.coreutils
+      ms-accept
+    ];
+    generator.script = ''
+      moonlight-sunshine-accept sunshine init
+      mv credentials/cakey.pem "$secrets"/sunshine.key
+      cp credentials/cacert.pem "$secrets"/sunshine.cert
+      mv credentials/cacert.pem "$facts"/sunshine.cert
+      mv uuid "$facts"/sunshine-uuid
+    '';
   };
 }
