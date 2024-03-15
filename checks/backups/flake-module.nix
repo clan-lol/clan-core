2377c11d465aa0698c15b14bc90b196af4589ae7
Jörg Thalheim <joerg@thalheim.io>
2024-03-15 14:06:50 +0000

    refactor backup test to be just a single machine

--- checks/backups/flake-module.nix
+++ checks/backups/flake-module.nix
@@ -3,51 +3,58 @@ let
   clan = self.lib.buildClan {
     clanName = "testclan";
     directory = ../..;
-    machines = {
-      test_backup_client = {
-        clan.networking.targetHost = "client";
-        imports = [ self.nixosModules.test_backup_client ];
-        fileSystems."/".device = "/dev/null";
-        boot.loader.grub.device = "/dev/null";
-      };
+    machines.test-backup = {
+      imports = [ self.nixosModules.test-backup ];
+      fileSystems."/".device = "/dev/null";
+      boot.loader.grub.device = "/dev/null";
     };
   };
 in
 {
-  flake.nixosConfigurations = { inherit (clan.nixosConfigurations) test_backup_client; };
   flake.clanInternals = clan.clanInternals;
   flake.nixosModules = {
-    test_backup_server = { ... }: {
-      imports = [
-        self.clanModules.borgbackup
-      ];
-      services.sshd.enable = true;
-      services.borgbackup.repos.testrepo = {
-        authorizedKeys = [
-          (builtins.readFile ../lib/ssh/pubkey)
-        ];
-      };
-    };
-    test_backup_client = { pkgs, lib, config, ... }:
+    test-backup = { pkgs, lib, config, ... }:
       let
         dependencies = [
           self
           pkgs.stdenv.drvPath
-          clan.clanInternals.machines.x86_64-linux.test_backup_client.config.system.clan.deployment.file
+          clan.clanInternals.machines.x86_64-linux.test-backup.config.system.clan.deployment.file
         ] ++ builtins.map (i: i.outPath) (builtins.attrValues self.inputs);
         closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
       in
       {
         imports = [
           self.clanModules.borgbackup
+          self.clanModules.sshd
         ];
+        clan.networking.targetHost = "test-backup";
+
+        services.borgbackup.repos.testrepo = {
+          authorizedKeys = [
+            (builtins.readFile ../lib/ssh/pubkey)
+          ];
+        };
+
         networking.hostName = "client";
         services.sshd.enable = true;
+
+        programs.ssh.knownHosts = {
+          machine.hostNames = [ "machine" ];
+          machine.publicKey = builtins.readFile ../lib/ssh/pubkey;
+        };
+
         users.users.root.openssh.authorizedKeys.keyFiles = [
           ../lib/ssh/pubkey
         ];
 
         systemd.tmpfiles.settings."vmsecrets" = {
+          "/etc/secrets/ssh.id_ed25519" = {
+            C.argument = "${../lib/ssh/privkey}";
+            z = {
+              mode = "0400";
+              user = "root";
+            };
+          };
           "/etc/secrets/borgbackup.ssh" = {
             C.argument = "${../lib/ssh/privkey}";
             z = {
@@ -75,7 +82,7 @@ in
         };
         system.extraDependencies = dependencies;
         clanCore.state.test-backups.folders = [ "/var/test-backups" ];
-        clan.borgbackup.destinations.test_backup_server.repo = "borg@server:.";
+        clan.borgbackup.destinations.test-backup.repo = "borg@machine:.";
       };
   };
   perSystem = { nodes, pkgs, ... }: {
@@ -84,55 +91,31 @@ in
         (import ../lib/test-base.nix)
           {
             name = "test-backups";
-            nodes.server = {
-              imports = [
-                self.nixosModules.test_backup_server
-                self.nixosModules.clanCore
-                {
-                  clanCore.machineName = "server";
-                  clanCore.clanDir = ../..;
-                }
-              ];
-            };
-            nodes.client = {
-              imports = [
-                self.nixosModules.test_backup_client
-                self.nixosModules.clanCore
-                {
-                  clanCore.machineName = "client";
-                  clanCore.clanDir = ../..;
-                }
-              ];
-            };
+            nodes.machine.imports = [
+              self.nixosModules.clanCore
+              self.nixosModules.test-backup
+              { clanCore.clanDir = ../..; }
+            ];
 
             testScript = ''
               import json
               start_all()
-
-              # setup
-              client.succeed("mkdir -m 700 /root/.ssh")
-              client.succeed(
-                  "cat ${../lib/ssh/privkey} > /root/.ssh/id_ed25519"
-              )
-              client.succeed("chmod 600 /root/.ssh/id_ed25519")
-              client.wait_for_unit("sshd", timeout=30)
-              client.succeed("ssh -o StrictHostKeyChecking=accept-new root@client hostname")
 
               # dummy data
               client.succeed("mkdir /var/test-backups")
               client.succeed("echo testing > /var/test-backups/somefile")
 
               # create
-              client.succeed("clan --debug --flake ${../..} backups create test_backup_client")
-              client.wait_until_succeeds("! systemctl is-active borgbackup-job-test_backup_server")
+              client.succeed("clan --debug --flake ${self} backups create test-backup")
+              client.wait_until_succeeds("! systemctl is-active borgbackup-job-test-backup")
 
               # list
-              backup_id = json.loads(client.succeed("borg-job-test_backup_server list --json"))["archives"][0]["archive"]
-              assert(backup_id in client.succeed("clan --debug --flake ${../..} backups list test_backup_client"))
+              backup_id = json.loads(client.succeed("borg-job-test-backup list --json"))["archives"][0]["archive"]
+              assert(backup_id in client.succeed("clan --debug --flake ${self} backups list test-backup"))
 
               # restore
               client.succeed("rm -f /var/test-backups/somefile")
-              client.succeed(f"clan --debug --flake ${../..} backups restore test_backup_client borgbackup {backup_id}")
+              client.succeed(f"clan --debug --flake ${self} backups restore test-backup borgbackup {backup_id}")
               assert(client.succeed("cat /var/test-backups/somefile").strip() == "testing")
             '';
           }
