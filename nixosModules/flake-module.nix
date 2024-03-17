13b69ca63a1cd698639ec4ce5b6b87f3c429f770
Jörg Thalheim <joerg@thalheim.io>
2024-03-17 19:38:50 +0100

    make installer nixos module stand-alone

--- nixosModules/flake-module.nix
+++ nixosModules/flake-module.nix
@@ -1,7 +1,11 @@
 { inputs, self, ... }: {
   flake.nixosModules = {
     hidden-ssh-announce.imports = [ ./hidden-ssh-announce.nix ];
-    installer.imports = [ ./installer ];
+    installer.imports = [
+      ./installer
+      self.nixosModules.hidden-ssh-announce
+      inputs.disko.nixosModules.disko
+    ];
     clanCore.imports = [
       inputs.sops-nix.nixosModules.sops
       ./clanCore
