d6d8a88549cd69148bdd54016cf73bacedb41765
Qubasa <consulting@qube.email>
2024-03-24 23:36:00 +0100

    clan-vm-manager: Working pytest skeleton. clan-cli: Fixing devshell depending on itself

--- pkgs/clan-vm-manager/tests/wayland.py
+++ pkgs/clan-vm-manager/tests/wayland.py
@@ -1,5 +1,7 @@
+import sys
 from collections.abc import Generator
 from subprocess import Popen
+from typing import NewType
 
 import pytest
 
@@ -7,18 +9,19 @@ import pytest
 @pytest.fixture(scope="session")
 def wayland_compositor() -> Generator[Popen, None, None]:
     # Start the Wayland compositor (e.g., Weston)
-    compositor = Popen(["weston", "--backend=headless-backend.so"])
+    # compositor = Popen(["weston", "--backend=headless-backend.so"])
+    compositor = Popen(["weston"])
     yield compositor
     # Cleanup: Terminate the compositor
     compositor.terminate()
 
 
+GtkProc = NewType("GtkProc", Popen)
+
+
 @pytest.fixture(scope="function")
-def gtk_app(wayland_compositor: Popen) -> Generator[Popen, None, None]:
-    # Assuming your GTK4 app can be started via a command line
-    # It's important to ensure it uses the Wayland session initiated by the fixture
-    env = {"GDK_BACKEND": "wayland"}
-    app = Popen(["clan-vm-manager"], env=env)
-    yield app
+def app() -> Generator[GtkProc, None, None]:
+    rapp = Popen([sys.executable, "-m", "clan_vm_manager"], text=True)
+    yield GtkProc(rapp)
     # Cleanup: Terminate your application
-    app.terminate()
+    rapp.terminate()
