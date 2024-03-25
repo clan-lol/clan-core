b175fd207b830f865d41d9c41940238df3e4145f
a-kenji <aks.kenji@protonmail.com>
2024-03-25 11:07:53 +0100

    s

--- pkgs/clan-cli/clan_cli/vms/waypipe.py
+++ pkgs/clan-cli/clan_cli/vms/waypipe.py
@@ -37,7 +37,7 @@ def start_waypipe(cid: int | None, title_prefix: str) -> Iterator[None]:
             "client",
         ],
     )
-    print(waypipe)
+    print(f"Waypipe command: {waypipe}")
     with subprocess.Popen(waypipe) as proc:
         try:
             while not test_vsock_port(3049):
