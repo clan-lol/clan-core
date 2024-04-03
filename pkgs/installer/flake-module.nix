321241070480b3637997b8f806d30ad788594a64
Jörg Thalheim <joerg@thalheim.io>
2024-04-03 13:46:17 +0200

    add flake-parts module for clan

--- pkgs/installer/flake-module.nix
+++ pkgs/installer/flake-module.nix
@@ -18,21 +18,21 @@ let
       { disko.memSize = 4096; } # FIXME: otherwise the image builder goes OOM
     ];
   };
-
-  clan = self.lib.buildClan {
-    clanName = "clan-core";
-    directory = self;
-    machines.installer = installerModule;
-  };
 in
 {
   flake.packages.x86_64-linux.install-iso = self.inputs.disko.lib.makeDiskImages {
     nixosConfig = installer;
   };
-  flake.nixosConfigurations = {
-    inherit (clan.nixosConfigurations) installer;
+
+  clan = {
+    clanName = "clan-core";
+    directory = self;
+    machines.installer = {
+      imports = [ installerModule ];
+      fileSystems."/".device = lib.mkDefault "/dev/null";
+      boot.loader.grub.device = lib.mkDefault "/dev/null";
+    };
   };
-  flake.clanInternals = clan.clanInternals;
   flake.apps.x86_64-linux.install-vm.program = installer.config.formats.vm.outPath;
   flake.apps.x86_64-linux.install-vm-nogui.program = installer.config.formats.vm-nogui.outPath;
 }
