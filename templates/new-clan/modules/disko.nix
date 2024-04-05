158a0d2d98330a1dacabc147c1d9f959f00a95dc
Jörg Thalheim <joerg@thalheim.io>
2024-04-05 13:08:32 +0200

    improve starter template

---
+++ templates/new-clan/modules/disko.nix
@@ -0,0 +1,36 @@
+{
+  config.disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = throw "Change this to your disk device";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
