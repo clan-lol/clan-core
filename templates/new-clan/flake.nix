e68eba914e7acd394501e53cb854693d75b8507f
Jörg Thalheim <joerg@thalheim.io>
2024-04-05 13:08:32 +0200

    improve starter template

--- templates/new-clan/flake.nix
+++ templates/new-clan/flake.nix
@@ -11,6 +11,45 @@
       clan = clan-core.lib.buildClan {
         directory = self;
         clanName = "__CHANGE_ME__";
+        machines = {
+          machine1 = {
+            nixpkgs.hostPlatform = system;
+            imports = [
+              # TODO: boot into the installer
+              # remote> nixos-generate-config --root /tmp/config --no-filesystems
+              # local> mkdir -p ./machines/machine1
+              # local> scp -r root@machine1:/tmp/config ./machines/machine1
+              # local> Edit ./machines/machine1/configuration.nix to your liking
+              ./modules/disko.nix
+              clan-core.clanModules.sshd
+              {
+                # Set this for clan commands use ssh i.e. `clan machines update`
+                clan.networking.targetHost = pkgs.lib.mkDefault "root@<IP_ADDRESS>";
+                # remote> lsblk --output NAME,PTUUID,FSTYPE,SIZE,MOUNTPOINT
+
+                config.disko.devices.disk.main.device = "/dev/disk/by-id/__CHANGE_ME__";
+
+                clan.networking.zerotier.controller.enable = true;
+              }
+            ];
+          };
+          machine2 = {
+            nixpkgs.hostPlatform = system;
+            imports = [
+              # ./machines/machine2/configuration.nix
+              ./modules/disko.nix
+              clan-core.clanModules.sshd
+              {
+                # Set this for clan commands use ssh i.e. `clan machines update`
+                clan.networking.targetHost = pkgs.lib.mkDefault "root@<IP_ADDRESS>";
+
+                config.disko.devices.disk.main.device = "/dev/disk/by-id/__CHANGE_ME__";
+                # local> clan facts generate
+                clan.networking.zerotier.networkId = builtins.readFile ./machines/machine1/facts/zerotier-network-id;
+              }
+            ];
+          };
+        };
       };
     in
     {
