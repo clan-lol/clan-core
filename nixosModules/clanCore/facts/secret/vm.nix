0d4e1f870b0356eca2518afda543e9210aa84a05
Jörg Thalheim <joerg@thalheim.io>
2024-04-09 18:04:26 +0200

    factstore: secret backends now can return the path to a secret dynamically
    try to move path function out

--- nixosModules/clanCore/facts/secret/vm.nix
+++ nixosModules/clanCore/facts/secret/vm.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   config = lib.mkIf (config.clanCore.facts.secretStore == "vm") {
-    clanCore.facts.secretDirectory = "/etc/secrets";
+    clanCore.facts.secretPathFunction = secret: "/etc/secrets/${secret.config.name}";
     clanCore.facts.secretUploadDirectory = "/etc/secrets";
     clanCore.facts.secretModule = "clan_cli.facts.secret_modules.vm";
   };
