996fdd6c9cf2b78a10164454496e4e0324cd32e3
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-04-10 12:03:23 +0200

    templates: makes template working

--- templates/new-clan/machines/jon/configuration.nix
+++ templates/new-clan/machines/jon/configuration.nix
@@ -1,4 +1,15 @@
 { ... }:
 {
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    # IMPORTANT! Add your SSH key here
+    # e.g. > cat ~/.ssh/id_ed25519.pub
+    "<YOUR SSH_KEY>"
+  ];
+
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # Disable the default gnome apps to speed up deployment
+  services.gnome.core-utilities.enable = false;
 }
