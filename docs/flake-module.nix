c3f2c548a6adfa0cbb4c915433cde736e2c5fad9
Jörg Thalheim <joerg@thalheim.io>
2024-04-12 16:37:50 +0200

    add deploy script for homepage

--- docs/flake-module.nix
+++ docs/flake-module.nix
@@ -1,11 +1,17 @@
 { inputs, ... }:
 {
   perSystem =
-    { self', pkgs, ... }:
+    {
+      config,
+      self',
+      pkgs,
+      ...
+    }:
     {
       devShells.docs = pkgs.callPackage ./shell.nix { inherit (self'.packages) docs; };
       packages = {
         docs = pkgs.python3.pkgs.callPackage ./default.nix { inherit (inputs) nixpkgs; };
+        deploy-docs = pkgs.callPackage ./deploy-docs.nix { inherit (config.packages) docs; };
       };
     };
 }
