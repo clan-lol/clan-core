1051367ca42053f8916879337febcf2baf5de584
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-04-30 18:53:00 +0200

    add cli docs generator

--- pkgs/clan-cli/clan_cli/__init__.py
+++ pkgs/clan-cli/clan_cli/__init__.py
@@ -79,13 +79,17 @@ def create_parser(prog: str | None = None) -> argparse.ArgumentParser:
         "--flake",
         help="path to the flake where the clan resides in, can be a remote flake or local, can be set through the [CLAN_DIR] environment variable",
         default=get_clan_flake_toplevel_or_env(),
+        metavar="PATH",
         type=flake_path,
+        epilog="Default is dynamically determined based on the current directory.",
     )
 
     subparsers = parser.add_subparsers()
 
     parser_backups = subparsers.add_parser(
-        "backups", help="manage backups of clan machines"
+        "backups",
+        help="manage backups of clan machines",
+        description="manage backups of clan machines",
     )
     backups.register_parser(parser_backups)
 
