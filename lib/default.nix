d897ac390c356b760eeb08c8512bf67030c7d867
Qubasa <consulting@qube.email>
2024-05-02 16:34:11 +0200

    Remove unreferenced clan modules

--- lib/default.nix
+++ lib/default.nix
@@ -9,13 +9,5 @@
   modules = import ./description.nix { inherit clan-core; };
   buildClan = import ./build-clan { inherit clan-core lib nixpkgs; };
 
-  vm-user = ./vm-user;
-  graphical = ./graphical;
-  xfce-vm = {
-    imports = [
-      ./vm-user
-      ./graphical
-      ./xfce-vm
-    ];
-  };
+
 }
--- lib/graphical/default.nix
+++
@@ -1,1 +0,0 @@
-_: { fonts.enableDefaultPackages = true; }
