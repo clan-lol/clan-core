8ac3c62d5d8e551e95e04ea08aedf1b61436f057
Valentin Gagarin <valentin@gagarin.work>
2024-05-22 11:27:43 +0200

    use a custom installer for pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    git-hooks.url = "github:fricklerhandwerk/git-hooks";
+    git-hooks.flake = false;
   };
 
   outputs =
