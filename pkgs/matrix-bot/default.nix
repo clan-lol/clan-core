881196188c4bfc1e1ec3acaab62972bc2cd30bf8
Qubasa <consulting@qube.email>
2024-06-13 18:34:29 +0200

    matrix-bot: Init git commit summarizer

---
+++ pkgs/matrix-bot/default.nix
@@ -0,0 +1,36 @@
+{ python3, setuptools, ... }:
+
+
+let
+
+  pythonDependencies = [
+
+  ];
+
+  runtimeDependencies = [ ];
+
+  testDependencies =
+    runtimeDependencies ++ [
+    ];
+
+in
+python3.pkgs.buildPythonApplication {
+  name = "matrix-bot";
+  src = ./.;
+  format = "pyproject";
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = pythonDependencies;
+
+  passthru.testDependencies = testDependencies;
+
+  # Clean up after the package to avoid leaking python packages into a devshell
+  postFixup = ''
+    rm $out/nix-support/propagated-build-inputs
+  '';
+
+  meta.mainProgram = "matrix-bot";
+}
