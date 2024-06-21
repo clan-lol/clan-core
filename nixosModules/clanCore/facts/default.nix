5b351da16657a7523cb83040105179818d0b945a
Jörg Thalheim <joerg@thalheim.io>
2024-06-21 16:51:40 +0200

    fix fake-etc build on machines without sandbox
    If we have no sandbox enabled or on macos with sandbox enabled, /etc
    contains a lot more files than we actually want.
    Instead of copying some random files, we now just create those files
    ourself.

--- nixosModules/clanCore/facts/default.nix
+++ nixosModules/clanCore/facts/default.nix
@@ -123,13 +123,23 @@
 
                         # prepare sandbox user
                         mkdir -p /etc
-                        cp ${
-                          pkgs.runCommand "fake-etc" { } ''
-                            export PATH="${pkgs.coreutils}/bin"
-                            mkdir -p $out
-                            cp /etc/* $out/
-                          ''
-                        }/* /etc/
+
+                        cat > /etc/group <<EOF
+                        root:x:0:
+                        nixbld:!:$(id -g):
+                        nogroup:x:65534:
+                        EOF
+
+                        cat > /etc/passwd <<EOF
+                        root:x:0:0:Nix build user:/build:/noshell
+                        nixbld:x:$(id -u):$(id -g):Nix build user:/build:/noshell
+                        nobody:x:65534:65534:Nobody:/:/noshell
+                        EOF
+
+                        cat > /etc/hosts <<EOF
+                        127.0.0.1 localhost
+                        ::1 localhost
+                        EOF
 
                         ${config.script}
                       '';
