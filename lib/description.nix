3777a4cf0252a99a26d36fcaba69233733392e16
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-06-26 15:10:21 +0200

    Add toml frontmatter description to jsonschema

--- lib/description.nix
+++ lib/description.nix
@@ -1,33 +1,34 @@
-{ ... }:
-
+{ clan-core, lib }:
 rec {
-  # getReadme =
-  #   modulename:
-  #   let
-  #     readme = "${clan-core}/clanModules/${modulename}/README.md";
-  #     readmeContents =
-  #       if (builtins.pathExists readme) then
-  #         (builtins.readFile readme)
-  #       else
-  #         throw "No README.md found for module ${modulename}";
-  #   in
-  #   readmeContents;
+  getReadme =
+    modulename:
+    let
+      readme = "${clan-core}/clanModules/${modulename}/README.md";
+      readmeContents =
+        if (builtins.pathExists readme) then
+          (builtins.readFile readme)
+        else
+          throw "No README.md found for module ${modulename}";
+    in
+    readmeContents;
 
-  # getShortDescription =
-  #   modulename:
-  #   let
-  #     content = (getReadme modulename);
-  #     parts = lib.splitString "---" content;
-  #     description = builtins.head parts;
-  #     number_of_newlines = builtins.length (lib.splitString "\n" description);
-  #   in
-  #   if (builtins.length parts) > 1 then
-  #     if number_of_newlines > 4 then
-  #       throw "Short description in README.md for module ${modulename} is too long. Max 3 newlines."
-  #     else if number_of_newlines <= 1 then
-  #       throw "Missing short description in README.md for module ${modulename}."
-  #     else
-  #       description
-  #   else
-  #     throw "Short description delimiter `---` not found in README.md for module ${modulename}";
+  getShortDescription =
+    modulename:
+    let
+      content = getReadme modulename;
+      parts = lib.splitString "---" content;
+      # Partition the parts into the first part (the readme content) and the rest (the metadata)
+      parsed = builtins.partition ({ index }: if index >= 2 then false else true) (
+        lib.filter ({ index, ... }: index != 0) (lib.imap0 (index: part: { inherit index part; }) parts)
+      );
+
+      # Use this if the content is needed
+      # readmeContent = lib.concatMapStrings (v: "---" + v.part) parsed.wrong;
+
+      meta = builtins.fromTOML (builtins.head parsed.right).part;
+    in
+    if (builtins.length parts >= 3) then
+      meta.description
+    else
+      throw "Short description delimiter `---` not found in README.md for module ${modulename}";
 }
