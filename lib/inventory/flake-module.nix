1ec57a5c5d5ce85005d5db05465b8516bfd98a8a
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-06-26 17:19:19 +0200

    Inventory: add eval tests

--- lib/inventory/flake-module.nix
+++ lib/inventory/flake-module.nix
@@ -1,42 +1,81 @@
-{ self, ... }:
+{ self, inputs, ... }:
+let
+  inputOverrides = builtins.concatStringsSep " " (
+    builtins.map (input: " --override-input ${input} ${inputs.${input}}") (builtins.attrNames inputs)
+  );
+in
 {
-
   flake.inventory = import ./example.nix { inherit self; };
+
   perSystem =
-    { pkgs, config, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      system,
+      ...
+    }:
+    let
+      buildInventory = import ./build-inventory {
+        clan-core = self;
+        inherit lib;
+      };
+    in
     {
       devShells.inventory-schema = pkgs.mkShell {
-        inputsFrom = [ config.checks.inventory-schema-checks ];
+        inputsFrom = with config.checks; [
+          lib-inventory-schema
+          lib-inventory-eval
+        ];
       };
 
-      checks.inventory-schema-checks = pkgs.stdenv.mkDerivation {
-        name = "inventory-schema-checks";
-        src = ./.;
-        buildInputs = [ pkgs.cue ];
-        buildPhase = ''
-          echo "Running inventory tests..."
-          # Cue is easier to run in the same directory as the schema
-          cd spec
+      # Run: nix-unit --extra-experimental-features flakes --flake .#legacyPackages.x86_64-linux.evalTests
+      legacyPackages.evalTests = import ./tests {
+        inherit buildInventory;
+        clan-core = self;
+      };
 
-          echo "Export cue as json-schema..."
-          cue export --out openapi root.cue
+      checks = {
+        lib-inventory-eval = pkgs.runCommand "tests" { nativeBuildInputs = [ pkgs.nix-unit ]; } ''
+          export HOME="$(realpath .)"
 
-          echo "Validate test/*.json against inventory-schema..."
-
-          test_dir="../examples"
-          for file in "$test_dir"/*; do
-            # Check if the item is a file
-            if [ -f "$file" ]; then
-              # Print the filename
-              echo "Running test on: $file"
-
-              # Run the cue vet command
-              cue vet "$file" root.cue -d "#Root"
-            fi
-          done
+          nix-unit --eval-store "$HOME" \
+            --extra-experimental-features flakes \
+            ${inputOverrides} \
+            --flake ${self}#legacyPackages.${system}.evalTests
 
           touch $out
         '';
+
+        lib-inventory-schema = pkgs.stdenv.mkDerivation {
+          name = "inventory-schema-checks";
+          src = ./.;
+          buildInputs = [ pkgs.cue ];
+          buildPhase = ''
+            echo "Running inventory tests..."
+            # Cue is easier to run in the same directory as the schema
+            cd spec
+
+            echo "Export cue as json-schema..."
+            cue export --out openapi root.cue
+
+            echo "Validate test/*.json against inventory-schema..."
+
+            test_dir="../examples"
+            for file in "$test_dir"/*; do
+              # Check if the item is a file
+              if [ -f "$file" ]; then
+                # Print the filename
+                echo "Running test on: $file"
+
+                # Run the cue vet command
+                cue vet "$file" root.cue -d "#Root"
+              fi
+            done
+
+            touch $out
+          '';
+        };
       };
     };
 }
