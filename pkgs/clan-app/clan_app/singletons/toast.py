bb9058f5ef86f41b5142e2f4f78a4cb5935eada5
Qubasa <consulting@qube.email>
2024-07-09 17:35:00 +0200

    clan-app: Remove vm-manager from codebase

--- pkgs/clan-app/clan_app/singletons/toast.py
+++ pkgs/clan-app/clan_app/singletons/toast.py
@@ -10,7 +10,6 @@ gi.require_version("Adw", "1")
 from gi.repository import Adw
 
 from clan_app.singletons.use_views import ViewStack
-from clan_app.views.logs import Logs
 
 log = logging.getLogger(__name__)
 
@@ -47,35 +46,6 @@ class ToastOverlay:
             self.overlay.add_toast(toast)
             toast.connect("dismissed", lambda toast: self.active_toasts.remove(key))
 
-
-class ErrorToast:
-    toast: Adw.Toast
-
-    def __init__(
-        self, message: str, persistent: bool = False, details: str = ""
-    ) -> None:
-        super().__init__()
-        self.toast = Adw.Toast.new(
-            f"""<span foreground='red'>❌ Error </span> {message}"""
-        )
-        self.toast.set_use_markup(True)
-
-        self.toast.set_priority(Adw.ToastPriority.HIGH)
-        self.toast.set_button_label("Show more")
-
-        if persistent:
-            self.toast.set_timeout(0)
-
-        views = ViewStack.use().view
-
-        # we cannot check this type, python is not smart enough
-        logs_view: Logs = views.get_child_by_name("logs")  # type: ignore
-        logs_view.set_message(details)
-
-        self.toast.connect(
-            "button-clicked",
-            lambda _: views.set_visible_child_name("logs"),
-        )
 
 
 class WarningToast:
