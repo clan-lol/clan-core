d21d296c5c697e479e866a8eb0adf255e0e834ac
Jörg Thalheim <joerg@thalheim.io>
2024-07-21 21:16:07 +0200

    rework installation template based on received feedback

--- templates/new-clan/modules/shared.nix
+++ templates/new-clan/modules/shared.nix
@@ -1,7 +1,28 @@
-{ clan-core, ... }:
+{ config, clan-core, ... }:
 {
   imports = [
+    # Enables the OpenSSH server for remote access
     clan-core.clanModules.sshd
+    # Set a root password
     clan-core.clanModules.root-password
+    clan-core.clanModules.user-password
   ];
+
+  # Locale service discovery and mDNS
+  services.avahi.enable = true;
+
+  # generate a random password for our user below
+  # can be read using `clan secrets get <machine-name>-user-password` command
+  clan.user-password.user = "user";
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+      "input"
+    ];
+    uid = 1000;
+    openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys;
+  };
 }
