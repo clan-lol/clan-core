1907200d5894a32edb16fe65d5ddd4766ff90af8
Jörg Thalheim <joerg@thalheim.io>
2024-07-24 22:03:12 +0200

    satisfy treefmt

--- templates/flake.nix
+++ templates/flake.nix
@@ -1,14 +1,16 @@
 {
-  outputs = { ... }: {
-    templates = {
-      default = {
-        description = "Initialize a new clan flake";
-        path = ./new-clan;
-      };
-      minimal = {
-        description = "for clans managed via (G)UI";
-        path = ./minimal;
+  outputs =
+    { ... }:
+    {
+      templates = {
+        default = {
+          description = "Initialize a new clan flake";
+          path = ./new-clan;
+        };
+        minimal = {
+          description = "for clans managed via (G)UI";
+          path = ./minimal;
+        };
       };
     };
-  };
 }
