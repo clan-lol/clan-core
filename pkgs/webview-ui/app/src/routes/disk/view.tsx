fa0f393cbb351812927aa30a3944e7c4e85cc653
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-07-29 17:05:06 +0200

    Clan-app: edit clan, memoize active clan

--- pkgs/webview-ui/app/src/routes/disk/view.tsx
+++ pkgs/webview-ui/app/src/routes/disk/view.tsx
@@ -2,24 +2,24 @@ import { callApi } from "@/src/api";
 import { activeURI } from "@/src/App";
 import { createQuery } from "@tanstack/solid-query";
 import { createEffect } from "solid-js";
+import toast from "solid-toast";
 
 export function DiskView() {
   const query = createQuery(() => ({
-    queryKey: ["disk", activeURI],
+    queryKey: ["disk", activeURI()],
     queryFn: async () => {
       const currUri = activeURI();
       if (currUri) {
         // Example of calling an API
         const result = await callApi("get_inventory", { base_path: currUri });
         if (result.status === "error") throw new Error("Failed to fetch data");
-
         return result.data;
       }
     },
   }));
   createEffect(() => {
     // Example debugging the data
-    console.log(query.data);
+    console.log(query);
   });
   return (
     <div>
