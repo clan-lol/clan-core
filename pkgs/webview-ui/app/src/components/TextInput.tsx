7e9214053f329abd1cf41e425e750e89affe200a
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-08-07 12:16:06 +0200

    Clan-app: Flash improve form & file input

---
+++ pkgs/webview-ui/app/src/components/TextInput.tsx
@@ -0,0 +1,54 @@
+import { FieldValues, FormStore, ResponseData } from "@modular-forms/solid";
+import { type JSX } from "solid-js";
+
+interface TextInputProps<T extends FieldValues, R extends ResponseData> {
+  formStore: FormStore<T, R>;
+  value: string;
+  inputProps: JSX.HTMLAttributes<HTMLInputElement>;
+  label: JSX.Element;
+  error?: string;
+  required?: boolean;
+  inlineLabel?: JSX.Element;
+}
+
+export function TextInput<T extends FieldValues, R extends ResponseData>(
+  props: TextInputProps<T, R>,
+) {
+  return (
+    <label
+      class="form-control w-full"
+      aria-disabled={props.formStore.submitting}
+    >
+      <div class="label">
+        <span
+          class="label-text block"
+          classList={{
+            "after:ml-0.5 after:text-primary after:content-['*']":
+              props.required,
+          }}
+        >
+          {props.label}
+        </span>
+      </div>
+
+      <div class="input input-bordered flex items-center gap-2">
+        {props.inlineLabel}
+        <input
+          {...props.inputProps}
+          value={props.value}
+          type="text"
+          class="grow"
+          classList={{
+            "input-disabled": props.formStore.submitting,
+          }}
+          placeholder="name"
+          required
+          disabled={props.formStore.submitting}
+        />
+      </div>
+      {props.error && (
+        <span class="label-text-alt font-bold text-error">{props.error}</span>
+      )}
+    </label>
+  );
+}
