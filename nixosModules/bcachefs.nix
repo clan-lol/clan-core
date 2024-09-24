17dbf07be7e04842712ac4a63b7f38f54e75a38e
Jörg Thalheim <joerg@thalheim.io>
2024-09-24 11:37:48 +0200

    fix zfs latest again

--- nixosModules/bcachefs.nix
+++ nixosModules/bcachefs.nix
@@ -1,15 +1,11 @@
 {
   lib,
   pkgs,
-  config,
   ...
 }:
 {
   # If we also need zfs, we can use the unstable version as we otherwise don't have a new enough kernel version
   boot.zfs.package = pkgs.zfsUnstable;
-  boot.kernelPackages = lib.mkIf config.boot.zfs.enabled (
-    lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages
-  );
 
   # Enable bcachefs support
   boot.supportedFilesystems.bcachefs = lib.mkDefault true;
--- nixosModules/installer/default.nix
+++ nixosModules/installer/default.nix
@@ -47,6 +47,7 @@ in
     (modulesPath + "/profiles/installation-device.nix")
     (modulesPath + "/profiles/all-hardware.nix")
     (modulesPath + "/profiles/base.nix")
+    ./zfs-latest.nix
   ];
 
   environment.systemPackages = [ pkgs.nixos-facter ];
