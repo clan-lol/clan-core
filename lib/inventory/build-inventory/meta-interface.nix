f169b1bcd14551937fee94f5d430ed25e6f4f787
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-10-04 14:30:50 +0200

    Meta.name: not nullable, since deduped

--- lib/inventory/build-inventory/meta-interface.nix
+++ lib/inventory/build-inventory/meta-interface.nix
@@ -3,7 +3,14 @@ let
   types = lib.types;
 
   metaOptions = {
-    name = lib.mkOption { type = types.nullOr types.str; };
+    name = lib.mkOption {
+      type = types.str;
+      description = ''
+        Name of the clan.
+
+        Needs to be (globally) unique, as this determines the folder name where the flake gets downloaded to.
+      '';
+    };
     description = lib.mkOption {
       default = null;
       type = types.nullOr types.str;
