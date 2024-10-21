1d7913aeb728e5ff5552f8895ddfc39178f27bdd
Timo <example@example.com>
2024-10-21 16:05:13 +0200

    ui/sidebar: updates component and introduces new styles (e.g. colors, typograhy)

--- pkgs/webview-ui/app/src/components/Sidebar/SidebarHeader.tsx
+++ pkgs/webview-ui/app/src/components/Sidebar/SidebarHeader.tsx
@@ -1,18 +1,33 @@
-import {SidebarFlyout} from './SidebarFlyout'
+import {createSignal} from 'solid-js'
+
 import {Typography} from '@/src/components/Typography'
+import {SidebarFlyout} from './SidebarFlyout'
 
 interface SidebarHeader {
     clanName?: string
 }
 
 export const SidebarHeader = (props:SidebarHeader)=>{
-    const {clanName} = props
+    const {clanName} = props;
+
+    const [showFlyout, toggleFlyout] = createSignal(false)
+
+    function handleClick(){
+        toggleFlyout(!showFlyout())
+    }
     
     return <header class="sidebar__header">
-        <div class="sidebar__header__inner">
-            <div class="sidebar__profile">P</div>
+        <div onClick={handleClick} class="sidebar__header__inner">
+            <div class={`sidebar__profile ${showFlyout() ? 'sidebar__profile--flyout' : ''}`}>
+                <Typography classes='sidebar__title' tag='span' hierarchy='title' size='m' weight='bold' color='primary' inverted={!showFlyout()}>
+                    {clanName?.slice(0,1) || 'U'}
+                </Typography>    
+            </div>
             <Typography classes='sidebar__title' tag='h3' hierarchy='body' size='default' weight='medium' color='primary' inverted={true}>{clanName || 'Untitled'}</Typography>
         </div>
-        <SidebarFlyout/>
+        { showFlyout() &&
+            <SidebarFlyout/>
+        }
+        
     </header>
 }
--- pkgs/webview-ui/app/src/components/Sidebar/SidebarListItem.tsx
+++ pkgs/webview-ui/app/src/components/Sidebar/SidebarListItem.tsx
@@ -1,3 +1,5 @@
+import {Typography} from '@/src/components/Typography'
+
 interface SidebarListItem {
     title:string;
     delegateClick: () => void;
@@ -7,5 +9,6 @@ export const SidebarListItem = (props: SidebarListItem) =>{
     const {title} = props;
 
     return <li class="sidebar__list__item">
-        <span class="sidebar__list__content">{title}</span></li>
+            <Typography classes='sidebar__list__content' tag='span' hierarchy='body' size='default' weight='normal' color="primary" inverted={true}>{title}</Typography>              
+         </li>
 }
