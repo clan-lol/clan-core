bae360b3b76d607ca69914ca932a650aaedfb4aa
Jörg Thalheim <joerg@thalheim.io>
2024-10-23 09:44:39 +0200

    remove unused nixos-images flake input

--- flake.nix
+++ flake.nix
@@ -8,13 +8,10 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.inputs.nixpkgs-stable.follows = "";
-    nixos-images.url = "github:nix-community/nixos-images";
-    nixos-images.inputs.nixos-unstable.follows = "nixpkgs";
-    # unused input
-    nixos-images.inputs.nixos-stable.follows = "";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
+
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
