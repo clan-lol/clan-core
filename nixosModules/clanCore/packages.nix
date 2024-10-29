dec610cc033753bda775039c5be3138a164053da
Jörg Thalheim <joerg@thalheim.io>
2024-10-29 13:26:08 +0100

    add nixos-facter to base system

--- nixosModules/clanCore/packages.nix
+++ nixosModules/clanCore/packages.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [
-    # essential debugging tools for networked services
-    pkgs.dnsutils
-    pkgs.tcpdump
-    pkgs.curl
-    pkgs.jq
-    pkgs.htop
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  environment.systemPackages =
+    [
+      # essential debugging tools for networked services
+      pkgs.dnsutils
+      pkgs.tcpdump
+      pkgs.curl
+      pkgs.jq
+      pkgs.htop
+
+    pkgs.nixos-facter # for `clan machines update-hardware-config --backend nixos-facter`
     # needed to deploy via `clan machines update` if the flake has a git input
     pkgs.gitMinimal
   ];
