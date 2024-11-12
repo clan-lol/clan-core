83e2367b221e4ee12419b34fd816ef7220b3a440
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-11-12 14:46:16 +0100

    Chore: fix export of machines clan schema

--- nixosModules/clanCore/schema.nix
+++ nixosModules/clanCore/schema.nix
@@ -6,7 +6,7 @@ in
   options.clanSchema = lib.mkOption {
     type = lib.types.attrs;
     description = "The json schema for the .clan options namespace";
-    default = jsonschema.parseOptions options.clan;
+    default = jsonschema.parseOptions options.clan { };
     defaultText = lib.literalExpression "jsonschema.schemaToJSON options.clan";
   };
 }
