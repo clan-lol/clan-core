4759b9528fdef6b6c42e7e726b38ba2866f9b547
Jörg Thalheim <joerg@thalheim.io>
2024-11-14 17:52:02 +0100

    also deploy host ca to server

--- clanModules/sshd/roles/client.nix
+++ clanModules/sshd/roles/client.nix
@@ -3,9 +3,4 @@
   imports = [
     ../shared.nix
   ];
-  programs.ssh.knownHosts.ssh-ca = lib.mkIf (config.clan.sshd.certificate.searchDomains != [ ]) {
-    certAuthority = true;
-    extraHostNames = builtins.map (domain: "*.${domain}") config.clan.sshd.certificate.searchDomains;
-    publicKey = config.clan.core.vars.generators.openssh-ca.files."id_ed25519.pub".value;
-  };
 }
--- clanModules/sshd/shared.nix
+++ clanModules/sshd/shared.nix
@@ -39,5 +39,11 @@
             ssh-keygen -t ed25519 -N "" -f $out/id_ed25519
           '';
         };
+
+    programs.ssh.knownHosts.ssh-ca = lib.mkIf (config.clan.sshd.certificate.searchDomains != [ ]) {
+      certAuthority = true;
+      extraHostNames = builtins.map (domain: "*.${domain}") config.clan.sshd.certificate.searchDomains;
+      publicKey = config.clan.core.vars.generators.openssh-ca.files."id_ed25519.pub".value;
+    };
   };
 }
