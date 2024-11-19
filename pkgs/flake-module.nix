e5184eb5d48a6c5f1d373ad8dd6baa34111add34
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-11-19 17:59:30 +0100

    chore(packages/schema): remove unused validation

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -6,7 +6,6 @@
     ./clan-app/flake-module.nix
     ./clan-vm-manager/flake-module.nix
     ./installer/flake-module.nix
-    ./schemas/flake-module.nix
     ./webview-ui/flake-module.nix
     ./distro-packages/flake-module.nix
     ./icon-update/flake-module.nix
