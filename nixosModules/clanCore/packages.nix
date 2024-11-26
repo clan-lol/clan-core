b732855b0431a5fba2615bab5f060c0e4f755d3b
Jörg Thalheim <joerg@thalheim.io>
2024-11-26 12:57:31 +0100

    always include gitMinimal
    the condition was actually the wrong way around and we still need git in
    nix for some operations.

--- nixosModules/clanCore/packages.nix
+++ nixosModules/clanCore/packages.nix
@@ -1,23 +1,18 @@
 {
   pkgs,
-  lib,
-  config,
   ...
 }:
 {
-  environment.systemPackages =
-    [
-      # essential debugging tools for networked services
-      pkgs.dnsutils
-      pkgs.tcpdump
-      pkgs.curl
-      pkgs.jq
-      pkgs.htop
+  environment.systemPackages = [
+    # essential debugging tools for networked services
+    pkgs.dnsutils
+    pkgs.tcpdump
+    pkgs.curl
+    pkgs.jq
+    pkgs.htop
 
-      pkgs.nixos-facter # for `clan machines update-hardware-config --backend nixos-facter`
-    ]
-    ++ lib.optional (lib.versionAtLeast config.nix.package.version "2.24")
-      # needed to deploy via `clan machines update` if the flake has a git input
-      # newer version of nix do have `libgit2`
-      pkgs.gitMinimal;
+    pkgs.nixos-facter # for `clan machines update-hardware-config --backend nixos-facter`
+
+    pkgs.gitMinimal
+  ];
 }
