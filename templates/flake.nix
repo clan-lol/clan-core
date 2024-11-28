e4310e96c7827f8502f2f3d1c06885b67cdd38b4
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-11-28 11:17:43 +0100

    Templates: move all clan templates into subfolder 'clan'

--- templates/flake.nix
+++ templates/flake.nix
@@ -5,23 +5,23 @@
       templates = {
         default = {
           description = "Initialize a new clan flake";
-          path = ./new-clan;
+          path = ./clan/new-clan;
         };
         minimal = {
           description = "for clans managed via (G)UI";
-          path = ./minimal;
+          path = ./clan/minimal;
         };
         flake-parts = {
           description = "Flake-parts";
-          path = ./flake-parts;
+          path = ./clan/flake-parts;
         };
         minimal-flake-parts = {
           description = "Minimal flake-parts clan template";
-          path = ./minimal-flake-parts;
+          path = ./clan/minimal-flake-parts;
         };
         machineTemplates = {
           description = "Machine templates";
-          path = ./machineTemplates;
+          path = ./clan/machineTemplates;
         };
       };
     };
