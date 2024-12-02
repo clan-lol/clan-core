06f0e495cec61790bf2d8d105268ab9f944460ac
DavHau <d.hauer.it@gmail.com>
2024-12-02 15:50:25 +0700

    zerotier: improve docs for moons

--- clanModules/zerotier/roles/moon.nix
+++ clanModules/zerotier/roles/moon.nix
@@ -10,6 +10,9 @@
       Other machines can join this moon by adding this moon in their config.
       It will be reachable under the given stable endpoints.
     '';
+    example = ''
+      [ 1.2.3.4" "10.0.0.3/9993" "2001:abcd:abcd::3/9993" ]
+    '';
   };
   # TODO, we want to remove these options from clanCore
   config.clan.core.networking.zerotier.moon.stableEndpoints =
