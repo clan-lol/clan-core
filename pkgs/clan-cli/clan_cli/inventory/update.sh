e1c135bf9d0fce97bfa67dfb3ec61e0ad3e39e60
Jörg Thalheim <joerg@thalheim.io>
2024-12-04 17:15:54 +0100

    clan-cli/clan_cli/inventory/update.sh: make script idempotent

--- pkgs/clan-cli/clan_cli/inventory/update.sh
+++ pkgs/clan-cli/clan_cli/inventory/update.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 jsonSchema=$(nix build .#schemas.inventory-schema-abstract --print-out-paths)/schema.json
-nix run .#classgen  "$jsonSchema" "$PKG_ROOT/clan_cli/inventory/classes.py" -- --stop-at "Service"
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR"
+nix run .#classgen -- "$jsonSchema" "../../../clan-cli/clan_cli/inventory/classes.py" --stop-at "Service"
