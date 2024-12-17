0330ee39a3b26b1488e3182b7da3d73994bdeccf
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-12-17 13:43:25 +0100

    UI: init label typography

--- pkgs/webview-ui/app/tailwind/typography.ts
+++ pkgs/webview-ui/app/tailwind/typography.ts
@@ -10,6 +10,9 @@ export const typography: Partial<Config["theme"]> = {
     title: ["1.125rem", { lineHeight: "124%" }],
     "title-m": ["1.25rem", { lineHeight: "124%" }],
     "title-l": ["1.375rem", { lineHeight: "124%" }],
+    label: ["0.8125rem", { lineHeight: "100%" }],
+    "label-s": ["0.75rem", { lineHeight: "100%" }],
+    "label-xs": ["0.6875rem", { lineHeight: "124%" }],
   },
   // textColor: {
   //   ...defaultTheme.textColor,
