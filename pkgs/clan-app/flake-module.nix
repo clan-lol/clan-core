93a657aca6427bd2186679ad90a1c3623271ec53
Qubasa <consulting@qube.email>
2025-01-11 10:52:11 +0700

    clan-app: allow darwin build

--- pkgs/clan-app/flake-module.nix
+++ pkgs/clan-app/flake-module.nix
@@ -4,21 +4,19 @@
     {
       config,
       pkgs,
-      lib,
-      system,
       self',
       ...
     }:
-      {
-        devShells.clan-app = pkgs.callPackage ./shell.nix {
-          inherit (config.packages) clan-app webview-lib;
-          inherit self';
-        };
-        packages.clan-app = pkgs.callPackage ./default.nix {
-          inherit (config.packages) clan-cli webview-ui webview-lib;
-          pythonRuntime = pkgs.python3;
-        };
-
-        checks = config.packages.clan-app.tests;
+    {
+      devShells.clan-app = pkgs.callPackage ./shell.nix {
+        inherit (config.packages) clan-app webview-lib;
+        inherit self';
       };
+      packages.clan-app = pkgs.callPackage ./default.nix {
+        inherit (config.packages) clan-cli webview-ui webview-lib;
+        pythonRuntime = pkgs.python3;
+      };
+
+      checks = config.packages.clan-app.tests;
+    };
 }
