db417ecd13e7ebbf9ae7d20ac0f11776d96e882c
lassulus <git@lassul.us>
2025-01-28 10:09:15 +0100

    clan-cli vars upload: add optional --directory

--- pkgs/clan-cli/clan_cli/vars/upload.py
+++ pkgs/clan-cli/clan_cli/vars/upload.py
@@ -1,6 +1,7 @@
 import argparse
 import importlib
 import logging
+from pathlib import Path
 
 from clan_cli.completions import add_dynamic_completer, complete_machines
 from clan_cli.machines.machines import Machine
@@ -8,15 +9,21 @@ from clan_cli.machines.machines import Machine
 log = logging.getLogger(__name__)
 
 
-def upload_secret_vars(machine: Machine) -> None:
+def upload_secret_vars(machine: Machine, directory: Path | None = None) -> None:
     secret_store_module = importlib.import_module(machine.secret_vars_module)
     secret_store = secret_store_module.SecretStore(machine=machine)
-    secret_store.upload(phases=["activation", "users", "services"])
+    if directory:
+        secret_store.populate_dir(directory, phases=["activation", "users", "services"])
+    else:
+        secret_store.upload(phases=["activation", "users", "services"])
 
 
 def upload_command(args: argparse.Namespace) -> None:
     machine = Machine(name=args.machine, flake=args.flake)
-    upload_secret_vars(machine)
+    directory = None
+    if args.directory:
+        directory = Path(args.directory)
+    upload_secret_vars(machine, directory)
 
 
 def register_upload_parser(parser: argparse.ArgumentParser) -> None:
@@ -26,4 +33,8 @@ def register_upload_parser(parser: argparse.ArgumentParser) -> None:
     )
     add_dynamic_completer(machines_parser, complete_machines)
 
+    parser.add_argument(
+        "--directory",
+        help="If provided, secrets will be copied into this directory instead of the remote server",
+    )
     parser.set_defaults(func=upload_command)
