2fb312ccc19e09d16cfa3f057c91eeafee0bae5a
a-kenji <aks.kenji@protonmail.com>
2025-02-02 12:38:15 +0700

    clanModules/root-password: Add `inventory` feature

--- clanModules/root-password/README.md
+++ clanModules/root-password/README.md
@@ -1,5 +1,7 @@
 ---
 description = "Automatically generates and configures a password for the root user."
+categories = ["System"]
+features = [ "inventory" ]
 ---
 
 After the system was installed/deployed the following command can be used to display the root-password:
--- clanModules/root-password/default.nix
+++ clanModules/root-password/default.nix
@@ -1,35 +1,6 @@
+# Dont import this file
+# It is only here for backwards compatibility.
+# Dont author new modules with this file.
 {
-  pkgs,
-  config,
-  ...
-}:
-{
-  users.mutableUsers = false;
-  users.users.root.hashedPasswordFile =
-    config.clan.core.vars.generators.root-password.files.password-hash.path;
-
-  clan.core.vars.generators.root-password = {
-    files.password-hash = {
-      neededFor = "users";
-    };
-    migrateFact = "root-password";
-    runtimeInputs = [
-      pkgs.coreutils
-      pkgs.mkpasswd
-      pkgs.xkcdpass
-    ];
-    prompts.password.type = "hidden";
-    prompts.password.persist = true;
-    prompts.password.description = "You can autogenerate a password, if you leave this prompt blank.";
-
-    script = ''
-      prompt_value=$(cat $prompts/password)
-      if [[ -n ''${prompt_value-} ]]; then
-        echo $prompt_value | tr -d "\n" > $out/password
-      else
-        xkcdpass --numwords 3 --delimiter - --count 1 | tr -d "\n" > $out/password
-      fi
-      mkpasswd -s -m sha-512 < $out/password | tr -d "\n" > $out/password-hash
-    '';
-  };
+  imports = [ ./roles/default.nix ];
 }
