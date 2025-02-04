fdb3f894806ba86ca8037aeb0d6d88ae41d4d9ac
Qubasa <consulting@qube.email>
2025-02-04 17:10:25 +0700

    clan-app: Fix Gsettings schema files not found

--- pkgs/clan-app/shell.nix
+++ pkgs/clan-app/shell.nix
@@ -1,9 +1,7 @@
 {
-  gsettings-desktop-schemas,
   clan-app,
   mkShell,
   ruff,
-  gtk4,
   webview-lib,
   self',
 }:
@@ -39,8 +37,7 @@ mkShell {
     # Add clan-cli to the python path so that we can import it without building it in nix first
     export PYTHONPATH="$GIT_ROOT/pkgs/clan-cli":"$PYTHONPATH"
 
-    export XDG_DATA_DIRS=${gtk4}/share/gsettings-schemas/gtk4-4.14.4:$XDG_DATA_DIRS
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/gsettings-desktop-schemas-46.0:$XDG_DATA_DIRS
+    export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH:$XDG_DATA_DIRS
 
     export WEBVIEW_LIB_DIR=${webview-lib}/lib
     source $PKG_ROOT/.local.env
