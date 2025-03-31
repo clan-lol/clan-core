b31cf42d2dc7cc4cfb8bf859b6583a294f4caae3
Brian McGee <brian@bmcgee.ie>
2025-03-31 10:50:57 +0100

    fix(docs): tighten up validation for meta.name and improved description

--- lib/inventory/build-inventory/meta-interface.nix
+++ lib/inventory/build-inventory/meta-interface.nix
@@ -4,11 +4,14 @@ let
 
   metaOptions = {
     name = lib.mkOption {
-      type = types.str;
+      type = types.strMatching "[a-zA-Z0-9_-]*";
+      example = "my_clan";
       description = ''
         Name of the clan.
 
         Needs to be (globally) unique, as this determines the folder name where the flake gets downloaded to.
+
+        Should only contain alphanumeric characters, `_` and `-`.
       '';
     };
     description = lib.mkOption {
