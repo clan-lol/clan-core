b6d8888d8c9e572644f0ba0432079cd920d1bade
Brian McGee <brian@bmcgee.ie>
2025-04-08 15:44:59 +0100

    chore: remove data-mesher from devshell

--- devShell.nix
+++ devShell.nix
@@ -1,12 +1,10 @@
-{ inputs, ... }:
+{ ... }:
 {
   perSystem =
     {
-      lib,
       pkgs,
       self',
       config,
-      system,
       ...
     }:
     let
@@ -26,26 +24,18 @@
     in
     {
       devShells.default = pkgs.mkShell {
-        packages =
-          [
-            select-shell
-            pkgs.nix-unit
-            pkgs.tea
-            # Better error messages than nix 2.18
-            pkgs.nixVersions.latest
-            self'.packages.tea-create-pr
-            self'.packages.merge-after-ci
-            self'.packages.pending-reviews
-            # treefmt with config defined in ./flake-parts/formatting.nix
-            config.treefmt.build.wrapper
-          ]
-          # bring in data-mesher for the cli which can help with things like key generation
-          ++ (
-            let
-              data-mesher = inputs.data-mesher.packages.${system}.data-mesher or null;
-            in
-            lib.optional (data-mesher != null) data-mesher
-          );
+        packages = [
+          select-shell
+          pkgs.nix-unit
+          pkgs.tea
+          # Better error messages than nix 2.18
+          pkgs.nixVersions.latest
+          self'.packages.tea-create-pr
+          self'.packages.merge-after-ci
+          self'.packages.pending-reviews
+          # treefmt with config defined in ./flake-parts/formatting.nix
+          config.treefmt.build.wrapper
+        ];
         shellHook = ''
           echo -e "${ansiEscapes.green}switch to another dev-shell using: select-shell${ansiEscapes.reset}"
           export PRJ_ROOT=$(git rev-parse --show-toplevel)
