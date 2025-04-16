e21326054e4b99ff9514d2ee9f6e29dd074e91a7
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-04-16 15:07:57 +0200

    refactor(clanServices): add example of eval test

--- clanServices/flake-module.nix
+++ clanServices/flake-module.nix
@@ -1,8 +1,5 @@
-{ self, lib, ... }:
 {
-  clan.inventory.modules = {
-    zerotier-redux = lib.modules.importApply ./zerotier-redux/default.nix {
-      inherit (self) packages;
-    };
-  };
+  imports = [
+    ./zerotier-redux/flake-module.nix
+  ];
 }
