1ca79610e0122604ca619a70371569921b96583c
DavHau <d.hauer.it@gmail.com>
2025-04-21 12:47:27 +0700

    container-tests: add multi-container network

--- checks/container/default.nix
+++ checks/container/default.nix
@@ -3,17 +3,42 @@
   {
     name = "container";
 
-    nodes.machine =
+    nodes.machine1 =
       { ... }:
       {
-        networking.hostName = "machine";
+        networking.hostName = "machine1";
         services.openssh.enable = true;
         services.openssh.startWhenNeeded = false;
       };
+
+    nodes.machine2 =
+      { ... }:
+      {
+        networking.hostName = "machine2";
+        services.openssh.enable = true;
+        services.openssh.startWhenNeeded = false;
+      };
+
     testScript = ''
+      import subprocess
       start_all()
-      machine.succeed("systemctl status sshd")
-      machine.wait_for_unit("sshd")
+      machine1.succeed("systemctl status sshd")
+      machine2.succeed("systemctl status sshd")
+      machine1.wait_for_unit("sshd")
+      machine2.wait_for_unit("sshd")
+
+      p1 = subprocess.run(["ip", "a"], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      assert p1.returncode == 0
+      bridge_output = p1.stdout.decode("utf-8")
+      assert "br0" in bridge_output, f"bridge not found in ip a output: {bridge_output}"
+
+      for m in [machine1, machine2]:
+          out = machine1.succeed("ip addr show eth1")
+          assert "UP" in out, f"UP not found in ip addr show output: {out}"
+          assert "inet" in out, f"inet not found in ip addr show output: {out}"
+          assert "inet6" in out, f"inet6 not found in ip addr show output: {out}"
+
+      machine1.succeed("ping -c 1 machine2")
     '';
   }
 )
--- checks/lib/container-test.nix
+++ checks/lib/container-test.nix
@@ -4,49 +4,52 @@ let
   inherit (pkgs) lib;
   nixos-lib = import (pkgs.path + "/nixos/lib") { };
 in
-(nixos-lib.runTest (
-  { hostPkgs, ... }:
-  {
-    hostPkgs = pkgs;
-    # speed-up evaluation
-    defaults =
-      { config, options, ... }:
-      {
-        imports = [
-          self.clanLib.test.minifyModule
-        ];
-        config = lib.mkMerge [
-          (lib.optionalAttrs (options ? clan) {
-            clan.core.settings.machine.name = config.networking.hostName;
-          })
-          {
-            documentation.enable = lib.mkDefault false;
-            boot.isContainer = true;
+(nixos-lib.runTest {
+  hostPkgs = pkgs;
+  # speed-up evaluation
+  defaults =
+    { config, options, ... }:
+    {
+      imports = [
+        self.clanLib.test.minifyModule
+      ];
+      config = lib.mkMerge [
+        (lib.optionalAttrs (options ? clan) {
+          clan.core.settings.machine.name = config.networking.hostName;
+        })
+        {
+          documentation.enable = lib.mkDefault false;
+          boot.isContainer = true;
 
-            # needed since nixpkgs 7fb2f407c01b017737eafc26b065d7f56434a992 removed the getty unit by default
-            console.enable = true;
+          # needed since nixpkgs 7fb2f407c01b017737eafc26b065d7f56434a992 removed the getty unit by default
+          console.enable = true;
 
-            # undo qemu stuff
-            system.build.initialRamdisk = "";
-            virtualisation.sharedDirectories = lib.mkForce { };
-            networking.useDHCP = false;
+          # undo qemu stuff
+          system.build.initialRamdisk = "";
+          virtualisation.sharedDirectories = lib.mkForce { };
+          networking.useDHCP = false;
 
-            # we have not private networking so far
-            networking.interfaces = lib.mkForce { };
-            #networking.primaryIPAddress = lib.mkForce null;
-            systemd.services.backdoor.enable = false;
+          # We use networkd to assign static ip addresses
+          networking.useNetworkd = true;
+          services.resolved.enable = false;
 
-            # we don't have permission to set cpu scheduler in our container
-            systemd.services.nix-daemon.serviceConfig.CPUSchedulingPolicy = lib.mkForce "";
-          }
-        ];
-      };
-    # to accept external dependencies such as disko
-    node.specialArgs.self = self;
-    _module.args = { inherit self; };
-    imports = [
-      test
-      ./container-driver/module.nix
-    ];
-  }
-)).config.result
+          # Rename the host0 interface to eth0 to match what we expect in VM tests.
+          system.activationScripts.renameInterface = ''
+            ${pkgs.iproute2}/bin/ip link set dev host0 name eth1
+          '';
+
+          systemd.services.backdoor.enable = false;
+
+          # we don't have permission to set cpu scheduler in our container
+          systemd.services.nix-daemon.serviceConfig.CPUSchedulingPolicy = lib.mkForce "";
+        }
+      ];
+    };
+  # to accept external dependencies such as disko
+  node.specialArgs.self = self;
+  _module.args = { inherit self; };
+  imports = [
+    test
+    ./container-driver/module.nix
+  ];
+}).config.result
