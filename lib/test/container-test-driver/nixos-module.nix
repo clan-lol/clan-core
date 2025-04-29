41d875cb313689bf01ce81421c01d056d37e066a
Michael Hoang <enzime@users.noreply.github.com>
2025-04-29 15:02:50 +1000

    container-test-driver: fix SSH not working inside container tests

--- lib/test/container-test-driver/nixos-module.nix
+++ lib/test/container-test-driver/nixos-module.nix
@@ -23,4 +23,13 @@
 
   # we don't have permission to set cpu scheduler in our container
   systemd.services.nix-daemon.serviceConfig.CPUSchedulingPolicy = lib.mkForce "";
+
+  # Adds `Include /nix/store/...` to `/etc/ssh/ssh_config`[1] which will make
+  # SSH fail when running inside a container test as SSH checks the permissions
+  # of the config files it reads which can't be disabled[2] and all the store
+  # paths inside the build sandbox (and the container by extension) are owned
+  # by `nobody:nogroup` rather than `root:nixbld`.
+  # [1]: https://github.com/NixOS/nixpkgs/blob/29335f23bea5e34228349ea739f31ee79e267b88/nixos/modules/programs/ssh.nix#L344-L347
+  # [2]: https://github.com/openssh/openssh-portable/blob/b5b405fee7f3e79d44e2d2971a4b6b4cc53f112e/readconf.c#L2579-L2587
+  programs.ssh.systemd-ssh-proxy.enable = false;
 }
