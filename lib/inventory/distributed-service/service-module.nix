38b76aa5e58cbde4c2f317fcdd721f9d5ce86b5d
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-05-10 13:58:21 +0200

    Refactor(inventory/manifest): move into seperate file

--- lib/inventory/distributed-service/service-module.nix
+++ lib/inventory/distributed-service/service-module.nix
@@ -232,43 +232,7 @@ in
       description = "Meta information about this module itself";
       type = submoduleWith {
         modules = [
-          {
-            options = {
-              name = mkOption {
-                description = ''
-                  The name of the module
-
-                  Mainly used to create an error context while evaluating.
-                  This helps backtracking which module was included; And where an error came from originally.
-                '';
-                type = types.str;
-              };
-              features = mkOption {
-                description = ''
-                  Enable built-in features for the module
-
-                  See the documentation for each feature:
-                  - API
-                '';
-                type = types.submoduleWith {
-                  modules = [
-                    {
-                      options.API = mkOption {
-                        type = types.bool;
-                        # This is read only, because we don't support turning it off yet
-                        readOnly = true;
-                        default = true;
-                        description = ''
-                          Enables automatic API schema conversion for the interface of this module.
-                        '';
-                      };
-                    }
-                  ];
-                };
-                default = { };
-              };
-            };
-          }
+          ./manifest/default.nix
         ];
       };
     };
