1cbc1d9af8e99565c3b042c34aa0c01487b6af8c
Jörg Thalheim <joerg@thalheim.io>
2025-05-28 20:25:51 +0200

    test_upload_single_file: add missing ssh_control_master

--- pkgs/clan-cli/clan_cli/tests/test_upload_single_file.py
+++ pkgs/clan-cli/clan_cli/tests/test_upload_single_file.py
@@ -7,7 +7,6 @@ from clan_lib.ssh.remote import Remote
 
 @pytest.mark.with_core
 def test_upload_single_file(
-    monkeypatch: pytest.MonkeyPatch,
     temporary_home: Path,
     hosts: list[Remote],
 ) -> None:
@@ -16,8 +15,8 @@ def test_upload_single_file(
     src_file = temporary_home / "test.txt"
     src_file.write_text("test")
     dest_file = temporary_home / "test_dest.txt"
-
-    upload(host, src_file, dest_file)
+    with host.ssh_control_master() as host:
+        upload(host, src_file, dest_file)
 
     assert dest_file.exists()
     assert dest_file.read_text() == "test"
