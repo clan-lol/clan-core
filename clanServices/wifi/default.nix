2cc0818c500d9ea21f4f72c6429ba48ee5f9db46
Jörg Thalheim <joerg@thalheim.io>
2025-05-30 12:09:30 +0200

    Revert "serivces/wifi: make generator names generic"
    This reverts commit af4b2f861b4dbd85b5fea583a0bb75db684c18be.

--- clanServices/wifi/default.nix
+++ clanServices/wifi/default.nix
@@ -46,13 +46,13 @@ in
           { pkgs, config, ... }:
           let
             password_path =
-              network_name: config.clan.core.vars.generators."wifi.${network_name}".files.password.path;
+              network_name: config.clan.core.vars.generators."iwd.${network_name}".files.password.path;
 
             ssid_path =
-              network_name: config.clan.core.vars.generators."wifi.${network_name}".files.network-name.path;
+              network_name: config.clan.core.vars.generators."iwd.${network_name}".files.network-name.path;
 
             secret_generator = name: value: {
-              name = "wifi.${name}";
+              name = "iwd.${name}";
               value = {
                 prompts.network-name.type = "line";
                 prompts.network-name.persist = true;
