41484d54433bbbd0cf5f8aaf9cfebb63cc41594e
pinpox <clan@pablo.tools>
2025-06-02 16:19:06 +0200

    Update clan-core-for-checks

--- checks/clan-core-for-checks.nix
+++ checks/clan-core-for-checks.nix
@@ -2,5 +2,5 @@
 fetchgit {
   url = "https://git.clan.lol/clan/clan-core.git";
   rev = "13a9b1719835ef4510e4adb6941ddfe9a91d41cb";
-  sha256 = "sha256-8zlAW+iWKrxxozSNEZAVlDPOMLzetd46RL1GLnAvvjg=";
+  sha256 = "sha256-M+pLnpuX+vIsxTFtbBZaNA1OwGQPeSbsMbTiDl1t4vY=";
 }
