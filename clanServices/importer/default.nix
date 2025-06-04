9bf0d133c89dfa5b88b765acf5c7282bba3c3d80
pinpox <clan@pablo.tools>
2025-06-04 14:31:07 +0200

    Add readme for clanServices/importer

--- clanServices/importer/default.nix
+++ clanServices/importer/default.nix
@@ -4,5 +4,7 @@
   manifest.name = "clan-core/importer";
   manifest.description = "Convenient, structured module imports for hosts.";
   manifest.categories = [ "Utility" ];
+  manifest.readme = builtins.readFile ./README.md;
+
   roles.default = { };
 }
