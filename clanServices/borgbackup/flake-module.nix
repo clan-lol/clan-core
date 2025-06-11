316ec8c526223ee60aec62cad75ce15a3cb4c8b4
a-kenji <aks.kenji@protonmail.com>
2025-06-11 13:47:29 +0200

    borgbackup: Move tests close to service

--- clanServices/borgbackup/flake-module.nix
+++ clanServices/borgbackup/flake-module.nix
@@ -1,6 +1,17 @@
-{ lib, ... }:
+{ lib, self, ... }:
 {
   clan.modules = {
     borgbackup = lib.modules.importApply ./default.nix { };
   };
+  perSystem =
+    { pkgs, ... }:
+    {
+      checks = lib.optionalAttrs (pkgs.stdenv.isLinux) {
+        borgbackup = import ./tests/vm/default.nix {
+          inherit pkgs;
+          clan-core = self;
+          nixosLib = import (self.inputs.nixpkgs + "/nixos/lib") { };
+        };
+      };
+    };
 }
