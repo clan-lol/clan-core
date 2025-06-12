25a610819877bceb978ddc5d5796d08571076dcc
Qubasa <consulting@qube.email>
2025-06-12 16:23:53 +0200

    clan-lib: Move task functions to own file

--- pkgs/clan-cli/clan_lib/api/directory.py
+++ pkgs/clan-cli/clan_lib/api/directory.py
@@ -33,20 +33,6 @@ class FileRequest:
 
 
 @API.register_abstract
-def cancel_task(task_id: str) -> None:
-    """Cancel a task by its op_key."""
-    msg = "cancel_task() is not implemented"
-    raise NotImplementedError(msg)
-
-
-@API.register_abstract
-def list_tasks() -> list[str]:
-    """List all tasks."""
-    msg = "list_tasks() is not implemented"
-    raise NotImplementedError(msg)
-
-
-@API.register_abstract
 def open_file(file_request: FileRequest) -> list[str] | None:
     """
     Abstract api method to open a file dialog window.
