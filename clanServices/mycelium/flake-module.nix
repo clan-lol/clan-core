3524b4a36827553df6ac0791d5df1ff167098e11
Jörg Thalheim <joerg@thalheim.io>
2025-06-17 19:30:21 +0200

    mycelium: migrate to clan.nixosTests module

--- clanServices/mycelium/flake-module.nix
+++ clanServices/mycelium/flake-module.nix
@@ -1,17 +1,18 @@
-{ lib, self, ... }:
+{ lib, ... }:
+let
+  module = lib.modules.importApply ./default.nix { };
+in
 {
   clan.modules = {
-    mycelium = lib.modules.importApply ./default.nix { };
+    mycelium = module;
   };
   perSystem =
-    { pkgs, ... }:
+    { ... }:
     {
-      checks = lib.optionalAttrs (pkgs.stdenv.isLinux) {
-        mycelium = import ./tests/vm/default.nix {
-          inherit pkgs;
-          clan-core = self;
-          nixosLib = import (self.inputs.nixpkgs + "/nixos/lib") { };
-        };
+      clan.nixosTests.mycelium = {
+        imports = [ ./tests/vm/default.nix ];
+
+        clan.modules."@clan/mycelium" = module;
       };
     };
 }
