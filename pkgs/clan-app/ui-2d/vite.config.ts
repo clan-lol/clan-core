9f08e5265c529ed743de39dbc41f23f558c06ee2
Qubasa <consulting@qube.email>
2025-06-18 20:56:52 +0200

    clan-app: Fix npm run build in ui-2d

--- pkgs/clan-app/ui-2d/vite.config.ts
+++ pkgs/clan-app/ui-2d/vite.config.ts
@@ -1,1 +1,54 @@
-../ui/vite.config.ts
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import solidSvg from "vite-plugin-solid-svg";
+import devtools from "solid-devtools/vite";
+import path from "node:path";
+import { exec } from "child_process";
+
+// watch also clan-cli to catch api changes
+const clanCliDir = path.resolve(__dirname, "../../clan-cli");
+
+function regenPythonApiOnFileChange() {
+  return {
+    name: "run-python-script-on-change",
+    handleHotUpdate({}) {
+      exec("reload-python-api.sh", (err, stdout, stderr) => {
+        if (err) {
+          console.error(`reload-python-api.sh error:\n${stderr}`);
+        }
+      });
+    },
+    configureServer(server: import("vite").ViteDevServer) {
+      server.watcher.add([clanCliDir]);
+    },
+  };
+}
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"), // This now points to ui-2d directory
+    },
+  },
+  optimizeDeps: {
+    include: ["debug", "extend"],
+  },
+  plugins: [
+    /*
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    devtools(),
+    solidPlugin(),
+    solidSvg(),
+    regenPythonApiOnFileChange(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "safari11",
+    //  assetsDi
+    manifest: true,
+  },
+});
---
+++ pkgs/clan-app/ui-2d/vitest.config.ts
@@ -0,0 +1,1 @@
+../ui/vitest.config.ts
