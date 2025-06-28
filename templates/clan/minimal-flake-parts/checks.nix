863aa9029c666552180518ab89e6dad0f040f098
Qubasa <consulting@qube.email>
2025-06-28 22:42:39 +0700

    Revert "Cleanup templates"
    This reverts commit 77d8f76eb88f678f952f265043de00fbc8d4ac61.

---
+++ templates/clan/minimal-flake-parts/checks.nix
@@ -0,0 +1,22 @@
+{ self, ... }:
+{
+  perSystem =
+    {
+      self',
+      lib,
+      system,
+      ...
+    }:
+    {
+      checks =
+        let
+          nixosMachines = lib.mapAttrs' (
+            name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel
+          ) ((lib.filterAttrs (_: config: config.pkgs.system == system)) self.nixosConfigurations);
+
+          packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+          devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
+        in
+        nixosMachines // packages // devShells;
+    };
+}
