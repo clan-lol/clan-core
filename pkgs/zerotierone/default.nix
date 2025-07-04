e383a8ddf489a57a4fe3820b8527cfc58ddbd24f
Jörg Thalheim <joerg@thalheim.io>
2025-07-04 18:19:47 +0200

    zerotierone: disable tests on macos

--- pkgs/zerotierone/default.nix
+++ pkgs/zerotierone/default.nix
@@ -1,7 +1,14 @@
-{ zerotierone, lib }:
+{
+  zerotierone,
+  stdenv,
+  lib,
+}:
 # halalify zerotierone
-zerotierone.overrideAttrs (_old: {
-  meta = _old.meta // {
+zerotierone.overrideAttrs (old: {
+  # Maybe a sandbox issue?
+  # zerotierone> [phy] Binding UDP listen socket to 127.0.0.1/60002... FAILED.
+  doCheck = old.doCheck && !stdenv.hostPlatform.isDarwin;
+  meta = old.meta // {
     license = lib.licenses.apsl20;
   };
 })
