3577c689bd563bfec3b14f6347dbd7a0be626f48
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-07-07 00:48:32 -0700

    Add missing f to f-string

--- pkgs/zerotier-members/zerotier-members.py
+++ pkgs/zerotier-members/zerotier-members.py
@@ -15,7 +15,7 @@ class ClanError(Exception):
 
 def compute_zerotier_ip(network_id: str, identity: str) -> ipaddress.IPv6Address:
     assert len(network_id) == 16, (
-        "network_id must be 16 characters long, got {network_id}"
+        f"network_id must be 16 characters long, got {network_id}"
     )
     nwid = int(network_id, 16)
     node_id = int(identity, 16)
