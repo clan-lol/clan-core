ff9f657ba51706cf71cf97abe74af2333693cba3
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-07-08 16:23:30 +0200

    UI: remove 2d-ui, its broken now since we deleted the symlinked files in #4266

--- pkgs/clan-app/ui-2d/src/Form/fields/FormSection.tsx
+++
@@ -1,8 +0,0 @@
-import { JSX } from "solid-js";
-
-interface FormSectionProps {
-  children: JSX.Element;
-}
-const FormSection = (props: FormSectionProps) => {
-  return <div class="p-2">{props.children}</div>;
-};
--- pkgs/clan-app/ui-2d/tailwind.config.ts
+++
@@ -1,1 +0,0 @@
-../ui/tailwind.config.ts
