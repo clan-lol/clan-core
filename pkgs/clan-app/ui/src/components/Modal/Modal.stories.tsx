0d487d19f5f2a56cd07619968fc4e1d3f448b771
Brian McGee <brian@bmcgee.ie>
2025-07-08 11:31:18 +0100

    feat(ui): prep V2
    Preparation for rebuilding the UI with V2 components.

---
+++ pkgs/clan-app/ui/src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,74 @@
+import { TagProps } from "@/src/components/Tag/Tag";
+import { Meta, StoryObj } from "@kachurun/storybook-solid";
+import { fn } from "storybook/test";
+import {
+  Modal,
+  ModalContext,
+  ModalProps,
+} from "@/src/components/Modal/Modal";
+import { Fieldset } from "@/src/components/Form/Fieldset";
+import { TextInput } from "@/src/components/Form/TextInput";
+import { TextArea } from "@/src/components/Form/TextArea";
+import { Checkbox } from "@/src/components/Form/Checkbox";
+import { Button } from "../Button/Button";
+
+const meta: Meta<ModalProps> = {
+  title: "Components/Modal",
+  component: Modal,
+};
+
+export default meta;
+
+type Story = StoryObj<TagProps>;
+
+export const Default: Story = {
+  args: {
+    title: "Example Modal",
+    onClose: fn(),
+    children: ({ close }: ModalContext) => (
+      <form class="flex flex-col gap-5">
+        <Fieldset legend="General">
+          {(props) => (
+            <>
+              <TextInput
+                {...props}
+                label="First Name"
+                size="s"
+                required={true}
+                input={{ placeholder: "Ron" }}
+              />
+              <TextInput
+                {...props}
+                label="Last Name"
+                size="s"
+                required={true}
+                input={{ placeholder: "Burgundy" }}
+              />
+              <TextArea
+                {...props}
+                label="Bio"
+                size="s"
+                input={{ placeholder: "Tell us a bit about yourself", rows: 8 }}
+              />
+              <Checkbox
+                {...props}
+                size="s"
+                label="Accept Terms"
+                required={true}
+              />
+            </>
+          )}
+        </Fieldset>
+
+        <div class="flex w-full items-center justify-end gap-4">
+          <Button size="s" hierarchy="secondary" onClick={close}>
+            Cancel
+          </Button>
+          <Button size="s" type="submit" hierarchy="primary" onClick={close}>
+            Save
+          </Button>
+        </div>
+      </form>
+    ),
+  },
+};
--- pkgs/clan-app/ui/src/components/v2/README.md
+++
@@ -1,7 +0,0 @@
-## Overview
-
-We will be updating existing components and developing new components in line with the latest designs inside this
-folder. As they become ready, they can be copied into the root `components` folder, replacing any existing components as
-necessary.
-
-This is to avoid merge hell and allow us to rapidly match the latest designs without the burden of integration.
