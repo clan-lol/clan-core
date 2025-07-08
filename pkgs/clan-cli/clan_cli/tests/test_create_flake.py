f8748d021b5d21ea39b6c4dcb62a4efe72319db1
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-07-08 13:03:32 +0200

    tests: dont expect .clan-flake file, it is not strictly required

--- pkgs/clan-cli/clan_cli/tests/test_create_flake.py
+++ pkgs/clan-cli/clan_cli/tests/test_create_flake.py
@@ -23,7 +23,6 @@ def test_create_flake(
 
     cli.run(["flakes", "create", str(flake_dir), "--template=default", "--no-update"])
 
-    assert (flake_dir / ".clan-flake").exists()
     # Replace the inputs.clan.url in the template flake.nix
     substitute(
         flake_dir / "flake.nix",
@@ -68,7 +67,6 @@ def test_create_flake_existing_git(
 
     cli.run(["flakes", "create", str(flake_dir), "--template=default", "--no-update"])
 
-    assert (flake_dir / ".clan-flake").exists()
     # Replace the inputs.clan.url in the template flake.nix
     substitute(
         flake_dir / "flake.nix",
