5e8faa144da600fd73365d94e6e97ef1a1b6879a
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-07-10 19:01:15 +0200

    templates/flake-parts: remove importing clanModules

--- templates/clan/flake-parts/modules/shared.nix
+++ templates/clan/flake-parts/modules/shared.nix
@@ -1,14 +1,6 @@
+{ ... }:
 {
-  clan-core,
-  # Optional, if you want to access other flakes:
-  # self,
-  ...
-}:
-{
-  imports = [
-    clan-core.clanModules.sshd
-    clan-core.clanModules.root-password
-    # You can access other flakes imported in your flake via `self` like this:
-    # self.inputs.nix-index-database.nixosModules.nix-index
-  ];
+  # Shared logic
+  # Can be imported into many machines
+
 }
