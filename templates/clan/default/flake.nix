38955f763fa136803de5dbb918e4095e0160f0be
DavHau <d.hauer.it@gmail.com>
2025-07-21 18:39:10 +0700

    clan default template: add inputs to specialArgs

--- templates/clan/default/flake.nix
+++ templates/clan/default/flake.nix
@@ -3,12 +3,18 @@
   inputs.nixpkgs.follows = "clan-core/nixpkgs";
 
   outputs =
-    { self, clan-core, ... }:
+    {
+      self,
+      clan-core,
+      nixpkgs,
+      ...
+    }@inputs:
     let
       # Usage see: https://docs.clan.lol
       clan = clan-core.lib.clan {
         inherit self;
         imports = [ ./clan.nix ];
+        specialArgs = { inherit inputs; };
       };
     in
     {
@@ -16,7 +22,7 @@
       # Add the Clan cli tool to the dev shell.
       # Use "nix develop" to enter the dev shell.
       devShells =
-        clan-core.inputs.nixpkgs.lib.genAttrs
+        nixpkgs.lib.genAttrs
           [
             "x86_64-linux"
             "aarch64-linux"
