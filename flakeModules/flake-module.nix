cbdf8745eb135549403ac293e7e62c6d9a63edb9
kurogeek <kurogeek@lmvhaus.com>
2025-07-23 12:07:03 +0700

    nixosTests option is exposed via flakeModules.testModule

--- flakeModules/flake-module.nix
+++ flakeModules/flake-module.nix
@@ -1,7 +1,22 @@
-{ self, config, ... }:
+{
+  self,
+  inputs,
+  config,
+  lib,
+  flake-parts-lib,
+  ...
+}:
 {
   flake.flakeModules = {
     clan = import ./clan.nix self;
     default = config.flake.flakeModules.clan;
+    testModule = import ../lib/flake-parts/clan-nixos-test.nix {
+      inherit
+        self
+        inputs
+        lib
+        flake-parts-lib
+        ;
+    };
   };
 }
