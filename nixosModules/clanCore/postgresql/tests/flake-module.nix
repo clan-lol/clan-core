65d194af58fd03d2641f0c59e16d8c8792c38013
pinpox <clan@pablo.tools>
2025-08-21 14:02:19 +0200

    Cleanup machine-id,postgresql test

--- nixosModules/clanCore/postgresql/tests/flake-module.nix
+++ nixosModules/clanCore/postgresql/tests/flake-module.nix
@@ -10,29 +10,13 @@
         clan = {
           directory = ./.;
 
-          # Workaround until we can use nodes.machine = { };
-          modules."@clan/importer" = ../../../../clanServices/importer;
-
-          inventory = {
-            machines.machine = { };
-            instances.importer = {
-              module.name = "@clan/importer";
-              module.input = "self";
-              roles.default.tags.all = { };
-              roles.default.extraModules = [
-                {
-                  clan.core.postgresql.enable = true;
-                  clan.core.postgresql.users.test = { };
-                  clan.core.postgresql.databases.test.create.options.OWNER = "test";
-                  clan.core.settings.directory = ./.;
-                }
-              ];
-            };
+          machines.machine = {
+            clan.core.postgresql.enable = true;
+            clan.core.postgresql.users.test = { };
+            clan.core.postgresql.databases.test.create.options.OWNER = "test";
+            clan.core.settings.directory = ./.;
           };
         };
-
-        # TODO: Broken. Use instead of importer after fixing.
-        # nodes.machine = { };
 
         testScript =
           let
