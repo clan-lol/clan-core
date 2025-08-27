f77456a12344bcefd9a59fbf64fe63f2b113a5c3
DavHau <d.hauer.it@gmail.com>
2025-08-27 12:50:59 +0700

    vars: simplify graph implementation, remove obsolete closure functions
    - full_closure is obsolete since it is the same as calling requested_closure with the full list of generators.
    - minimal_closure is obsolete as well. Since the recent addition of dependents to the closure via 3d2127ce1e56904499cb8fc63fa37e7e4637a43d it is essentially the same as the all_missing_closure

--- pkgs/clan-cli/clan_cli/vars/graph_test.py
+++ pkgs/clan-cli/clan_cli/vars/graph_test.py
@@ -5,6 +5,10 @@ from clan_cli.vars.generator import (
 from clan_cli.vars.graph import all_missing_closure, requested_closure
 
 
+def generator_names(generator: list[Generator]) -> list[str]:
+    return [gen.name for gen in generator]
+
+
 def test_required_generators() -> None:
     # Create generators with proper machine context
     machine_name = "test_machine"
@@ -33,9 +37,6 @@ def test_required_generators() -> None:
         generator.key: generator for generator in [gen_1, gen_2, gen_2a, gen_2b]
     }
 
-    def generator_names(generator: list[Generator]) -> list[str]:
-        return [gen.name for gen in generator]
-
     assert generator_names(requested_closure([gen_1.key], generators)) == [
         "gen_1",
         "gen_2",
@@ -58,8 +59,50 @@ def test_required_generators() -> None:
         "gen_2b",
     ]
 
-    assert generator_names(all_missing_closure(generators)) == [
+    assert generator_names(all_missing_closure(generators.keys(), generators)) == [
         "gen_2",
         "gen_2a",
         "gen_2b",
     ]
+
+
+def test_shared_generator_invalidates_multiple_machines_dependents() -> None:
+    # Create generators with proper machine context
+    machine_1 = "machine_1"
+    machine_2 = "machine_2"
+    shared_gen = Generator(
+        name="shared_gen",
+        dependencies=[],
+        machine=None,  # Shared generator
+    )
+    gen_1 = Generator(
+        name="gen_1",
+        dependencies=[shared_gen.key],
+        machine=machine_1,
+    )
+    gen_2 = Generator(
+        name="gen_2",
+        dependencies=[shared_gen.key],
+        machine=machine_2,
+    )
+
+    shared_gen.exists = False
+    gen_1.exists = True
+    gen_2.exists = True
+    generators: dict[GeneratorKey, Generator] = {
+        generator.key: generator for generator in [shared_gen, gen_1, gen_2]
+    }
+
+    assert generator_names(all_missing_closure(generators.keys(), generators)) == [
+        "shared_gen",
+        "gen_1",
+        "gen_2",
+    ], (
+        "All generators should be included in all_missing_closure due to shared dependency"
+    )
+
+    assert generator_names(requested_closure([shared_gen.key], generators)) == [
+        "shared_gen",
+        "gen_1",
+        "gen_2",
+    ], "All generators should be included in requested_closure due to shared dependency"
