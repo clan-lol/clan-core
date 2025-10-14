ffdcd9b41e1979e231f0648220449deebf9f6df7
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-10-14 11:18:42 +0200

    app: use geist font
    Drop Archivo, ArchivoSemicondensed, CommitMono
    in favour of only one

--- pkgs/clan-app/fonts.nix
+++ pkgs/clan-app/fonts.nix
@@ -3,68 +3,39 @@
   runCommand,
 }:
 let
-
-  archivo = {
-    # 400 -> Regular
-    regular = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/Archivo-Regular.woff2";
-      hash = "sha256-sxWvVJvHnZgKvgvKCmSOz6AQa+G/IFv57YCeY4HyTQo=";
-    };
-
-    # 500 -> Medium
-    medium = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/Archivo-Medium.woff2";
-      hash = "sha256-FalQLFry4jdHwuMr1zmxyG7UuI1rn1pd2cV8tmJetRs=";
-    };
-
-    # 600 -> SemiBold
-    semiBold = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/Archivo-SemiBold.woff2";
-      hash = "sha256-XrTdFFUNLIdxwgqJB/sX8Om89XqgF/vCz1cYl7I6QTU=";
-    };
+  geist-regular = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/Geist/webfonts/Geist-Regular.woff2";
+    hash = "sha256-bQfiUE+DYxYCLRa6WHmlIVNTwv98s4HaClOp9G3tXwo=";
   };
-
-  archivoSemi = {
-    # 400 -> Regular
-    regular = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/ArchivoSemiCondensed-Regular.woff2";
-      hash = "sha256-3PeB6tMpbYxR9JFyQ+yjpM7bAvZIjcJ4eBiHr9iV5p4=";
-    };
-
-    # 500 -> Medium
-    medium = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/ArchivoSemiCondensed-Medium.woff2";
-      hash = "sha256-IKaY3YhpmjMaIVUpwKRLd6eFiIihBoAP99I/pwmyll8=";
-    };
-
-    # 600 -> SemiBold
-    semiBold = fetchurl {
-      url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/webfonts/ArchivoSemiCondensed-SemiBold.woff2";
-      hash = "sha256-fOE+b+UeTRoj+sDdUWR1pPCZVn0ABy6FEDDmXrOA4LY=";
-    };
+  geist-medium = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/Geist/webfonts/Geist-Medium.woff2";
+    hash = "sha256-9Tw1DTi/7VIpsDtToLJb/XTZhOhItU9c1P17xPnZ02w=";
   };
-
-  commitMono = fetchurl {
-    url = "https://github.com/eigilnikolajsen/commit-mono/raw/0b3b192f035cdc8d1ea8ffb5463cc23d73d0b89f/src/fonts/fontlab/CommitMonoV143-VF.woff2";
-    hash = "sha256-80LKbD8ll+bA/NhLPz7WTTzlvbbQrxnRkNZFpVixzyk=";
+  geist-semibold = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/Geist/webfonts/Geist-SemiBold.woff2";
+    hash = "sha256-Dpia8fMSsLE0OFfUqit1jKM0S7t7Zt5LwQROjI8yVJk=";
   };
-  archivoSemi_ttf = fetchurl {
-    url = "https://github.com/Omnibus-Type/Archivo/raw/b5d63988ce19d044d3e10362de730af00526b672/fonts/ttf/ArchivoSemiCondensed-Medium.ttf";
-    hash = "sha256-Kot1CvKqnXW1VZ7zX2wYZEziSA/l9J0gdfKkSdBxZ0w=";
+  geist-mono-regular = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/GeistMono/webfonts/GeistMono-Regular.woff2";
+    hash = "sha256-SKbRHoKeTDBR+9JSXg9h68soGhUyBqGknnVYvq/+4xQ=";
   };
-
+  geist-mono-medium = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/GeistMono/webfonts/GeistMono-Medium.woff2";
+    hash = "sha256-V3lOUKezrGJ3h4WO8XxvBILIxhXzI7SaId5abnA5aQQ=";
+  };
+  geist-mono-semibold = fetchurl {
+    url = "https://github.com/vercel/geist-font/raw/7bce30121bdadfa8aadd5761b7fbda04d64c470e/fonts/GeistMono/webfonts/GeistMono-SemiBold.woff2";
+    hash = "sha256-MNhO7HLZSBBvE4Fqh2vah2RPbR7jZWBrZq0oXOXbCjo=";
+  };
 in
 runCommand "" { } ''
   mkdir -p $out
 
-  cp ${archivo.regular} $out/Archivo-Regular.woff2
-  cp ${archivo.medium} $out/Archivo-Medium.woff2
-  cp ${archivo.semiBold} $out/Archivo-SemiBold.woff2
+  cp ${geist-regular} $out/Geist-Regular.woff2
+  cp ${geist-medium} $out/Geist-Medium.woff2
+  cp ${geist-semibold} $out/Geist-SemiBold.woff2
 
-  cp ${archivoSemi.regular} $out/ArchivoSemiCondensed-Regular.woff2
-  cp ${archivoSemi.medium} $out/ArchivoSemiCondensed-Medium.woff2
-  cp ${archivoSemi.semiBold} $out/ArchivoSemiCondensed-SemiBold.woff2
-
-  cp ${commitMono} $out/CommitMonoV143-VF.woff2
-  cp ${archivoSemi_ttf} $out/ArchivoSemiCondensed-Medium.ttf
+  cp ${geist-mono-regular} $out/GeistMono-Regular.woff2
+  cp ${geist-mono-medium} $out/GeistMono-Medium.woff2
+  cp ${geist-mono-semibold} $out/GeistMono-SemiBold.woff2
 ''
