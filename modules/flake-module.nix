1f8c2a3722f7c92cedc313c7d90b3d78741cac8a
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-10-23 17:52:39 +0200

    modules: move clan docs into clan-module

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,24 +1,5 @@
 {
-  self,
-  ...
-}:
-{
   imports = [
     ./clan/flake-module.nix
   ];
-  perSystem =
-    {
-      pkgs,
-      lib,
-      ...
-    }:
-    let
-      jsonDocs = import ./eval-docs.nix {
-        inherit pkgs lib;
-        clan-core = self;
-      };
-    in
-    {
-      legacyPackages.clan-options = jsonDocs.optionsJSON;
-    };
 }
