2a9e4e7860f5a9412f8b727ce121f0c44b63ea46
Jörg Thalheim <joerg@thalheim.io>
2025-11-04 15:40:49 +0100

    zfsUnstable -> zfs_unstable
    nixpkgs has a new path for this.

--- nixosModules/bcachefs.nix
+++ nixosModules/bcachefs.nix
@@ -5,7 +5,7 @@
 }:
 {
   # If we also need zfs, we can use the unstable version as we otherwise don't have a new enough kernel version
-  boot.zfs.package = pkgs.zfsUnstable;
+  boot.zfs.package = pkgs.zfs_unstable or pkgs.zfsUnstable;
 
   # Enable bcachefs support
   boot.supportedFilesystems.bcachefs = lib.mkDefault true;
