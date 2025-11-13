18c909ada71f09d84b96e4a26296d5e5c06216b9
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-11-13 18:50:33 +0700

    lib/exports: improve error message

--- lib/exports.nix
+++ lib/exports.nix
@@ -78,7 +78,12 @@ let
     let
       parts = lib.splitString ":" scopeStr;
       checkedParts = lib.throwIf (lib.length parts != 4) ''
-        clanLib.exports.parseScope: invalid scope string format, expected 4 parts separated by 3 ":"
+        clanLib.exports.parseScope: invalid scope string format.
+        Got '${scopeStr}'
+
+        fix:
+        - use the provided 'mkExports' utility or
+        - use clanLib.exports.buildScopeKey
       '' (parts);
     in
     {
