e2dab6e06fb94e8f9f71902f38a7a0af540a1799
Christian Friedow <christian@friedow.com>
2025-12-03 15:02:16 +0100

    chore: remove old monitoring service

--- clanServices/monitoring/default.nix
+++ clanServices/monitoring/default.nix
@@ -2,27 +2,4 @@
 {
   _class = "clan.service";
   manifest.name = "clan-core/monitoring";
-  manifest.description = "Monitoring service for the nodes in your clan";
-  manifest.readme = builtins.readFile ./README.md;
-
-  roles.telegraf = {
-    description = "Placeholder role to apply the telegraf monitoring agent";
-    interface =
-      { lib, ... }:
-      {
-        options.allowAllInterfaces = lib.mkOption {
-          type = lib.types.nullOr lib.types.bool;
-          default = null;
-          description = "Deprecated. Has no effect.";
-        };
-
-        options.interfaces = lib.mkOption {
-          type = lib.types.nullOr (lib.types.listOf lib.types.str);
-          default = null;
-          description = "Deprecated. Has no effect.";
-        };
-      };
-  };
-
-  imports = [ ./telegraf.nix ];
 }
--- clanServices/monitoring/flake-module.nix
+++ clanServices/monitoring/flake-module.nix
@@ -1,25 +1,7 @@
-{
-  lib,
-  self,
-  ...
-}:
+{ ... }:
 let
   module = ./default.nix;
 in
 {
   clan.modules.monitoring = module;
-
-  perSystem =
-    { pkgs, ... }:
-    {
-      clan.nixosTests.monitoring = {
-        imports = [
-          (lib.modules.importApply ./tests/vm/default.nix {
-            inherit (self) packages;
-            inherit pkgs;
-          })
-        ];
-        clan.modules.monitoring = module;
-      };
-    };
 }
