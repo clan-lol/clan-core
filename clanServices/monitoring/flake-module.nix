59573fc77351946e278fb6bf3c44044be49b8c92
Christian Friedow <christian@friedow.com>
2025-12-04 17:05:19 +0100

    test: add integration test for the monitoring service

--- clanServices/monitoring/flake-module.nix
+++ clanServices/monitoring/flake-module.nix
@@ -4,4 +4,13 @@ let
 in
 {
   clan.modules.monitoring = module;
+
+  perSystem =
+    { ... }:
+    {
+      clan.nixosTests.monitoring = {
+        imports = [ ./tests/vm/default.nix ];
+        clan.modules.monitoring = module;
+      };
+    };
 }
---
+++ clanServices/monitoring/tests/vm/default.nix
@@ -0,0 +1,51 @@
+{
+  name = "monitoring";
+
+  clan = {
+    directory = ./.;
+    test.useContainers = true;
+    inventory = {
+      machines = {
+        machine1 = { };
+        machine2 = { };
+      };
+
+      instances.monitoring = {
+        module = {
+          name = "monitoring";
+          input = "self";
+        };
+
+        roles = {
+          client.tags.all = { };
+          server.machines.machine1.settings.grafana.enable = true;
+        };
+      };
+    };
+  };
+
+  nodes = {
+    machine1 = {
+      networking.domain = "clan";
+      security.acme.acceptTerms = true;
+      security.acme.defaults.email = "noreply@clan.lol";
+    };
+  };
+
+  testScript =
+    { ... }:
+    ''
+      start_all()
+
+      # Wait for both machines to be ready
+      machine1.wait_for_unit("multi-user.target")
+      machine2.wait_for_unit("multi-user.target")
+
+      machine1.wait_for_unit("alloy")
+      machine2.wait_for_unit("alloy")
+
+      machine1.wait_for_unit("loki")
+      machine1.wait_for_unit("mimir")
+      machine1.wait_for_unit("grafana")
+    '';
+}
