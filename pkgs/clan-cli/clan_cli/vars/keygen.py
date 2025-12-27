0a63676874e5601866be39f8e9a64b38e95bc4d5
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-12-27 08:21:29 +0100

    clan_lib/vars: move keygen to clan_lib

--- pkgs/clan-cli/clan_cli/vars/keygen.py
+++ pkgs/clan-cli/clan_cli/vars/keygen.py
@@ -1,5 +1,4 @@
 import argparse
-import getpass
 import logging
 import sys
 from pathlib import Path
@@ -7,42 +6,9 @@ from pathlib import Path
 from clan_cli.secrets.key import generate_key
 from clan_cli.secrets.sops import SopsKey, maybe_get_admin_public_keys
 from clan_cli.secrets.users import add_user
-from clan_lib.api import API
-from clan_lib.errors import ClanError
+from clan_lib.vars.keygen import create_secrets_user, get_user_or_default
 
 log = logging.getLogger(__name__)
-
-
-def _get_user_or_default(user: str | None) -> str:
-    """Get the user name, defaulting to the logged-in OS username if not provided."""
-    if user is None:
-        try:
-            user = getpass.getuser()
-        except Exception as e:
-            msg = "No user provided and could not determine logged-in OS username. Please provide an explicit username via argument"
-            raise ClanError(msg) from e
-    return user
-
-
-# TODO: Unify with "create clan" should be done automatically
-@API.register
-def create_secrets_user(
-    flake_dir: Path,
-    user: str | None = None,
-    force: bool = False,
-) -> None:
-    """Initialize sops keys for vars"""
-    user = _get_user_or_default(user)
-    pub_keys = maybe_get_admin_public_keys()
-    if not pub_keys:
-        pub_keys = [generate_key()]
-    # TODO set flake_dir=flake.path / "vars"
-    add_user(
-        flake_dir=flake_dir,
-        name=user,
-        keys=pub_keys,
-        force=force,
-    )
 
 
 def _select_keys_interactive(pub_keys: list[SopsKey]) -> list[SopsKey]:
@@ -76,7 +42,7 @@ def create_secrets_user_interactive(
     force: bool = False,
 ) -> None:
     """Initialize sops keys for vars interactively."""
-    user = _get_user_or_default(user)
+    user = get_user_or_default(user)
     pub_keys = maybe_get_admin_public_keys()
     if pub_keys:
         # let the user select which of the keys to use
