12910977ce8ca5f92cb5a527852ff8269615b4bb
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-01-22 15:35:06 +0100

    clan: deprecate access to non-public clan-core attributes
    in specialArgs of a 'clan' submodule.
    We forward 'clan-core' to the specialArgs of every nixOS-module.
    
    Downstream users should not rely on everything about clan-core beeing stable

--- modules/clan/default.nix
+++ modules/clan/default.nix
@@ -10,10 +10,36 @@
   - _module.args.clanLib: reference to lib.clan function
 */
 { clan-core }:
+{ lib, ... }:
+let
+  public = [
+    "clanLib"
+    "lib"
+    "clan"
+    "packages"
+    "nixosModules"
+    "darwinModules"
+    # This is not strictly public
+    # But annoying in traces, when evaluated by `lib.types.isType`
+    "_type"
+  ];
+  public-clan-core = builtins.mapAttrs (
+    attrName: v:
+    lib.warnIf (!builtins.elem attrName public) ''
+      Using deprecated clan-core attribute: '${attrName}'
+
+      Only these attributes are officially supported:
+        ${lib.concatStringsSep "\n  " public}
+
+      If you need '${attrName}', please file an issue explaining your use case:
+      https://git.clan.lol/clan/clan-core/issues
+    '' v
+  ) clan-core;
+in
 {
   _class = "clan";
   _module.args = {
-    inherit clan-core;
+    clan-core = public-clan-core;
     inherit (clan-core) clanLib;
   };
   imports = [
