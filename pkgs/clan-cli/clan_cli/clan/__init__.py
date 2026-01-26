0ea3d5ca879071db852c88017b584fc561323a72
a-kenji <aks.kenji@protonmail.com>
2026-01-26 09:45:38 +0100

    pkgs/cli: Remove `clan flakes inspect`
    Remove the already deprecated `clan flakes inspect` subcommand.
    
    Addresses one part of: #6374

--- pkgs/clan-cli/clan_cli/clan/__init__.py
+++ pkgs/clan-cli/clan_cli/clan/__init__.py
@@ -1,8 +1,6 @@
 # !/usr/bin/env python3
 import argparse
 
-from clan_cli.clan.inspect import register_inspect_parser
-
 from .create import register_create_parser
 
 
@@ -16,10 +14,3 @@ def register_parser(parser: argparse.ArgumentParser) -> None:
     )
     create_parser = subparser.add_parser("create", help="Create a clan")
     register_create_parser(create_parser)
-    inspect_parser = subparser.add_parser(
-        "inspect",
-        help="Inspect a clan",
-        description="""Inspects the flake and prints some medata.
-Deprecated - This command is marked for removal in the next release""",
-    )
-    register_inspect_parser(inspect_parser)
