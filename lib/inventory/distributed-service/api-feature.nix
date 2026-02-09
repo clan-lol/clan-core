clan-lol/clan-core

commit 410d0d05322cc6492a44dea080ca063d4e6b2655

modules: move input-mapping into inventoryClass submodule

modules/inventoryClass/default.nix
@@ -1,10 +1,38 @@
-{ lib, clanLib, config, ... }:
+{
+  lib,
+  clanLib,
+  config,
+  ...
+}:
 let
   inherit (lib) types mkOption;
   submodule = m: types.submoduleWith { modules = [ m ]; };
+
+  inspectModule =
+    inputName: moduleName: module:
+    let
+      eval = clanLib.evalService {
+        modules = [ module ];
+        prefix = [
+          inputName
+          "clan"
+          "modules"
+          moduleName
+        ];
+      };
+    in
+    {
+      manifest = eval.config.manifest;
+      roles = lib.mapAttrs (_n: v: { inherit (v) description; }) eval.config.roles;
+    };
+
 in
 {
   options = {
+    flakeInputs = mkOption {
+      type = types.raw;
+    };
+
     directory = mkOption {
       type = types.path;
     };
@@ -34,6 +62,51 @@ in
             "tags"
             "services"
           ];
+    };
+
+    staticModules = lib.mkOption {
+      readOnly = true;
+      type = lib.types.raw;
+
+      apply = moduleSet: lib.mapAttrs (inspectModule "<clan-core>") moduleSet;
+    };
+    modulesPerSource = lib.mkOption {
+      # { sourceName :: { moduleName :: {} }}
+      readOnly = true;
+      type = lib.types.raw;
+      default =
+        let
+          inputsWithModules = lib.filterAttrs (_inputName: v: v ? clan.modules) config.flakeInputs;
+        in
+        lib.mapAttrs (
+          inputName: v: lib.mapAttrs (inspectModule inputName) v.clan.modules
+        ) inputsWithModules;
+    };
+    moduleSchemas = lib.mkOption {
+      # { sourceName :: { moduleName :: { roleName :: Schema }}}
+      readOnly = true;
+      type = lib.types.raw;
+      default = lib.mapAttrs (
+        _inputName: moduleSet:
+        lib.mapAttrs (
+          _moduleName: module:
+          (clanLib.evalService {
+            modules = [ module ];
+            prefix = [ ];
+          }).config.result.api.schema
+        ) moduleSet
+      ) config.modulesPerSource;
+    };
+    templatesPerSource = lib.mkOption {
+      # { sourceName :: { moduleName :: {} }}
+      readOnly = true;
+      type = lib.types.raw;
+      default =
+        let
+          inputsWithTemplates = lib.filterAttrs (_inputName: v: v ? clan.templates) config.flakeInputs;
+        in
+        lib.mapAttrs (_inputName: v: lib.mapAttrs (_n: t: t) v.clan.templates) inputsWithTemplates;
+
     };
   };
 }

modules/clan/module.nix
@@ -229,11 +229,8 @@ in
           };
           imports = [
             ../inventoryClass/default.nix
-            (lib.modules.importApply ../inventoryClass/service-list-from-inputs.nix {
-              inherit flakeInputs clanLib;
-            })
             {
-              inherit inventory directory;
+              inherit inventory directory flakeInputs;
             }
             (
               let
@@ -252,7 +249,6 @@ in
                 machines = config.distributedServices.allMachines;
               }
             )
-            ../inventoryClass/inventory-introspection.nix
           ];
         };
 

commit e780add7fb420fa15a8a7beece2feb375ad664df

modules/jsonschema: fix module schema should contain settings
It seems this was broken 3 months ago in 410d0d0

lib/inventory/distributed-service/api-feature.nix
@@ -22,7 +22,14 @@
             distributor = { $schema" = "http://json-schema.org/<version>"; ... }
           }
         '';
-        default = lib.mapAttrs (_roleName: v: clanLib.jsonschema.fromModule v.interface) config.roles;
+        default = lib.mapAttrs (
+          _roleName: r:
+          clanLib.jsonschema.fromModule {
+            typePrefix = "";
+            input = true;
+            output = false;
+          } r.interface
+        ) config.roles;
       };
     };
   };

modules/inventoryClass/default.nix
@@ -134,16 +134,25 @@ in
       # { sourceName :: { moduleName :: { roleName :: Schema }}}
       readOnly = true;
       type = lib.types.raw;
-      default = lib.mapAttrs (
-        _inputName: moduleSet:
+      default =
+        let
+          inputsWithModules = lib.filterAttrs (_inputName: v: v ? clan.modules) config.flakeInputs;
+        in
         lib.mapAttrs (
-          _moduleName: module:
-          (clanLib.evalService {
-            modules = [ module ];
-            prefix = [ ];
-          }).config.result.api.schema
-        ) moduleSet
-      ) config.modulesPerSource;
+          inputName: v:
+          lib.mapAttrs (
+            moduleName: module:
+            (clanLib.evalService {
+              modules = [ module ];
+              prefix = [
+                inputName
+                "clan"
+                "modules"
+                moduleName
+              ];
+            }).config.result.api.schema
+          ) v.clan.modules
+        ) inputsWithModules;
     };
     templatesPerSource = lib.mkOption {
       # { sourceName :: { moduleName :: {} }}