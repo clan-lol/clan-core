42e7c2134b6ad786400c73bb4f2da7f729911ae7
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-02-18 18:46:24 +0100

    vars/log: fix logging should defensively coerce into string

--- pkgs/clan-cli/clan_lib/term_log/__init__.py
+++ pkgs/clan-cli/clan_lib/term_log/__init__.py
@@ -6,4 +6,4 @@ log = logging.getLogger(__name__)
 
 def log_prefixed(msg: str, *a: object, **kw: Any) -> None:
     prefix = kw.pop("prefix", "unknown")
-    log.info(msg, *a, extra={"command_prefix": prefix}, **kw)
+    log.info(msg, *a, extra={"command_prefix": str(prefix)}, **kw)
