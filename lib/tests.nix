clan-lol/clan-core

commit d388763c3f2189e30ec3774d20adbd57127c7d45

vars: filter /vars directory for evaluation via settings.directory
This is the most commonly used pattern that needs to be handled with care in clanServices
Unsafe usage of ../../vars may still be used. Worth noting that this has a risk of building state-machines unintentially

modules/clan/module.nix
@@ -116,7 +116,95 @@ let
       )
     ) supportedSystems
   );
+  # TODO: Remove extendModules
+  machinesFiltered = lib.genAttrs supportedSystems (
+    system:
+    lib.mapAttrs (
+      _name: machine:
+      machine.extendModules {
+        modules = [
+          { clan.core.settings.directory = lib.mkForce filteredDirectory; }
+          (lib.modules.importApply ../../nixosModules/machineModules/overridePkgs.nix {
+            pkgs = pkgsFor.${system};
+          })
+        ];
+      }
+    ) configurations
+  );
 
+  mkMachines =
+    {
+      clan-core,
+      clanConfig,
+      directory,
+    }:
+    lib.mkMerge [
+      # Create some modules for each machine
+      # These can depend on the 'name' and
+      # everything that can be derived from the machine 'name'
+      # i.e. by looking up the corresponding information in the 'inventory' or 'clan' submodule
+      (lib.mapAttrs (
+        name: v:
+        (
+          { ... }@args:
+          let
+            _class =
+              # _class was added in https://github.com/NixOS/nixpkgs/pull/395141
+              # Clan relies on it to determine which modules to load
+              # people need to use at least that version of nixpkgs
+              args._class or (throw ''
+                Your version of nixpkgs is incompatible with the latest clan.
+                Please update nixpkgs input to the latest nixos-unstable or nixpkgs-unstable.
+                Run:
+                  nix flake update nixpkgs
+              '');
+          in
+          {
+            imports = [
+              # Counterpart to /nixosModules/clanCore/dependencies.nix
+              # Dependencies from 'clan' scope injected into 'machine' modules
+              {
+                inherit clanConfig;
+              }
+              # TODO: deprecate these options in favor of dependencies above
+              (lib.modules.importApply ../../nixosModules/machineModules/forName.nix {
+                inherit (clanConfig.inventory) meta;
+                inherit
+                  name
+                  directory
+                  ;
+              })
+              # Import the correct 'core' module
+              # We assume either:
+              # - nixosModules (_class = nixos)
+              # - darwinModules (_class = darwin)
+              (lib.optionalAttrs (clan-core ? "${_class}Modules") clan-core."${_class}Modules".clanCore)
+            ]
+            ++ lib.optionals (_class == "nixos") (v.machineImports or [ ])
+            ++ lib.optionals (_class == "darwin") (v.darwinImports or [ ]);
+
+            # default hostname
+            networking.hostName = lib.mkDefault name;
+          }
+        )
+      ) clanConfig.clanInternals.inventoryClass.machines)
+
+      # The user can define some machine config here
+      # i.e. 'clan.machines.jon = ...'
+      clanConfig.machines
+    ];
+
+  # Filter the source
+  # Made available as 'settings.directory'
+  # This prevents lines like:
+  # builtins.readDir "${config.clan.core.settings.directory}/vars/per-machine";
+  # Direct path access is still possible. i.e.:
+  # builtins.readDir ../../vars/per-machine
+  filteredDirectory = builtins.path {
+    name = "source-filtered-no-vars";
+    path = directory;
+    filter = path: _type: !(lib.hasPrefix "${toString directory}/vars" (toString path));
+  };
 in
 {
   imports = [
@@ -171,62 +259,10 @@ in
   config = {
     inventory.meta = config.meta;
 
-    outputs.moduleForMachine = lib.mkMerge [
-      # Create some modules for each machine
-      # These can depend on the 'name' and
-      # everything that can be derived from the machine 'name'
-      # i.e. by looking up the corresponding information in the 'inventory' or 'clan' submodule
-      (lib.mapAttrs (
-        name: v:
-        (
-          { ... }@args:
-          let
-            _class =
-              # _class was added in https://github.com/NixOS/nixpkgs/pull/395141
-              # Clan relies on it to determine which modules to load
-              # people need to use at least that version of nixpkgs
-              args._class or (throw ''
-                Your version of nixpkgs is incompatible with the latest clan.
-                Please update nixpkgs input to the latest nixos-unstable or nixpkgs-unstable.
-                Run:
-                  nix flake update nixpkgs
-              '');
-          in
-          {
-            imports = [
-              # Counterpart to /nixosModules/clanCore/dependencies.nix
-              # Dependencies from 'clan' scope injected into 'machine' modules
-              {
-                clanConfig = config;
-              }
-
-              # TODO: deprecate these options in favor of dependencies above
-              (lib.modules.importApply ../../nixosModules/machineModules/forName.nix {
-                inherit (config.inventory) meta;
-                inherit
-                  name
-                  directory
-                  ;
-              })
-              # Import the correct 'core' module
-              # We assume either:
-              # - nixosModules (_class = nixos)
-              # - darwinModules (_class = darwin)
-              (lib.optionalAttrs (clan-core ? "${_class}Modules") clan-core."${_class}Modules".clanCore)
-            ]
-            ++ lib.optionals (_class == "nixos") (v.machineImports or [ ])
-            ++ lib.optionals (_class == "darwin") (v.darwinImports or [ ]);
-
-            # default hostname
-            networking.hostName = lib.mkDefault name;
-          }
-        )
-      ) config.clanInternals.inventoryClass.machines)
-
-      # The user can define some machine config here
-      # i.e. 'clan.machines.jon = ...'
-      config.machines
-    ];
+    outputs.moduleForMachine = mkMachines {
+      inherit clan-core directory;
+      clanConfig = config;
+    };
 
     specialArgs = {
       self = lib.mkDefault config.self;
@@ -310,6 +346,10 @@ in
 
       # machine specifics
       machines = configsPerSystem;
+      # machines with filtered 'directory'
+      # Needed for preventing cyclic dependency of:
+      # {directory}/vars -> generators -> {directory}/vars
+      machinesFiltered = machinesFiltered;
     };
   };
 }

lib/clanTest/flake-module.nix
@@ -77,6 +77,18 @@ in
           }
         ) machineModules
       );
+      machinesCrossFiltered =
+        lib.genAttrs [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]
+          (
+            system:
+            lib.mapAttrs (
+              _: module:
+              lib.nixosSystem {
+                inherit system;
+                modules = [ module ];
+              }
+            ) machineModules
+          );
 
       update-vars-script = "${
         self.packages.${hostPkgs.stdenv.hostPlatform.system}.clan-cli
@@ -234,6 +246,12 @@ in
                     description = "The machines built for all supported platforms";
                     readOnly = true;
                   };
+                  test.machinesCrossFiltered = mkOption {
+                    default = machinesCrossFiltered;
+                    type = types.raw;
+                    description = "The machines built for all supported platforms (with vars filtered)";
+                    readOnly = true;
+                  };
                   test.systemsFile = mkOption {
                     default = builtins.toFile "flake.systems.nix" ''
                       [ "${hostPkgs.stdenv.hostPlatform.system}" ]
@@ -292,7 +310,7 @@ in
         ];
 
         result = {
-          inherit update-vars machinesCross;
+          inherit update-vars machinesCross machinesCrossFiltered;
         };
       };
     };

modules/clan/top-level-interface.nix
@@ -338,6 +338,7 @@ in
           templates = lib.mkOption { type = lib.types.raw; };
 
           machines = lib.mkOption { type = lib.types.raw; };
+          machinesFiltered = lib.mkOption { type = lib.types.raw; };
 
           # attrsOf system submodules, system is injected into each submodule
           systems = lib.mkOption {

pkgs/clan-cli/clan_lib/flake/flake.py
@@ -803,7 +803,9 @@ class Flake:
     _cache: FlakeCache | None = field(init=False, default=None)
     _path: Path | None = field(init=False, default=None)
     _is_local: bool | None = field(init=False, default=None)
-    _cache_miss_stack_traces: list[str] = field(init=False, default_factory=list)
+    _cache_miss_stack_traces: list[tuple[str, str]] = field(
+        init=False, default_factory=list
+    )
 
     @classmethod
     def from_json(
@@ -828,7 +830,7 @@ class Flake:
     def _record_cache_miss(self, selector_info: str) -> None:
         """Record a cache miss with its stack trace."""
         stack_trace = "".join(traceback.format_stack())
-        self._cache_miss_stack_traces.append(f"{selector_info}\n{stack_trace}")
+        self._cache_miss_stack_traces.append((selector_info, stack_trace))
 
     @property
     def _cache_misses(self) -> int:

pkgs/clan-cli/clan_lib/nix_selectors.py
@@ -19,7 +19,8 @@ DEFAULT_MACHINE_PREFIX = "clanInternals.machines"
 # Context variable for machine selector prefix
 # Set this via machine_prefix_context() when working with a specific flake
 _machine_prefix: ContextVar[str] = ContextVar(
-    "machine_prefix", default=DEFAULT_MACHINE_PREFIX
+    "machine_prefix",
+    default=DEFAULT_MACHINE_PREFIX,
 )
 
 
@@ -47,6 +48,14 @@ def machine_prefix_context(prefix: str) -> Generator[None]:
 def get_machine_prefix() -> str:
     """Get the current machine selector prefix."""
     return _machine_prefix.get()
+
+
+# Results in
+# - clanInternals.machinesFiltered
+# - checks.x86_64-linux.test.machinesCrossFiltered
+def get_filtered_machine_prefix() -> str:
+    """Get the current machine selector prefix."""
+    return _machine_prefix.get() + "Filtered"
 
 
 type StaticSel = Callable[[], str]
@@ -163,7 +172,7 @@ def machine_state(system: str, machine: str) -> str:
 # Requires the monitoring/telegraf module to be enabled
 # @machine_selector
 def machine_telegraf_cert_path(system: str, machine: str) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{machine}.config.clan.core.vars.generators.telegraf-certs.files.crt.path"
 
 
@@ -195,13 +204,13 @@ def machine_vm_inspect(system: str, machine: str) -> str:
 
 @machine_selector
 def machine_vars_settings_secret_module(system: str, machine: str) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{machine}.config.clan.core.vars.settings.secretModule"
 
 
 @machine_selector
 def machine_vars_settings_public_module(system: str, machine: str) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{machine}.config.clan.core.vars.settings.publicModule"
 
 
@@ -210,13 +219,13 @@ def machine_vars_settings_public_module(system: str, machine: str) -> str:
 
 @machines_selector
 def vars_generators_metadata(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.generators.*.{{share,dependencies,prompts,validationHash}}"
 
 
 @machines_selector
 def vars_generators_files(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.generators.*.files.*.{{secret,deploy,owner,group,mode,neededFor}}"
 
 
@@ -228,31 +237,31 @@ def vars_sops_default_groups(system: str, machines: list[str]) -> str:
 
 @machines_selector
 def vars_settings_public_module(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.settings.publicModule"
 
 
 @machines_selector
 def vars_settings_secret_module(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.settings.secretModule"
 
 
 @machines_selector
 def vars_sops_secret_upload_dir(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.?sops.?secretUploadDirectory"
 
 
 @machines_selector
 def vars_password_store_pass_command(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.?password-store.?passCommand"
 
 
 @machines_selector
 def vars_password_store_secret_location(system: str, machines: list[str]) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f"{prefix}.{system}.{{{','.join(machines)}}}.config.clan.core.vars.?password-store.?secretLocation"
 
 
@@ -267,5 +276,5 @@ def deployment_require_explicit_update(system: str, machines: list[str]) -> str:
 
 @generator_selector
 def generator_final_script(system: str, machine: str, generator: str) -> str:
-    prefix = get_machine_prefix()
+    prefix = get_filtered_machine_prefix()
     return f'{prefix}.{system}.{machine}.config.clan.core.vars.generators."{generator}".finalScript'

commit 58f37fcd6306fa262a2e3a4dd49d7c74a8ea0421

vars: fix public vars need access to .value
Fixes regression introduced in d388763c3f2189e30ec3774d20adbd57127c7d45

lib/cycle-test-flake/vars/per-machine/test-machine/my-gen/my-public-file/value
@@ -1,0 +1,1 @@
+hello-from-var

lib/cycle-test-flake/vars/per-machine/test-machine/my-gen/my-secret-file/secret
@@ -1,0 +1,1 @@
+hello-from-var

lib/tests.nix
@@ -311,6 +311,121 @@ in
       ];
     };
 
+  # Regression test: machinesFiltered must still allow reading public var values.
+  # The builtins.path filter that strips vars/ from settings.directory must not
+  # prevent direct file reads of committed var values via varsDirectory.
+  test_machines_filtered_reads_public_vars =
+    let
+      eval = clan {
+        self = {
+          inputs = { };
+        };
+        directory = ./cycle-test-flake;
+        meta.name = "test";
+
+        machines.test-machine = {
+          nixpkgs.hostPlatform = "x86_64-linux";
+
+          clan.core.vars.settings.publicStore = "in_repo";
+
+          clan.core.vars.generators.my-gen = {
+            # Same as in the next test
+            files.my-public-file = {
+              secret = false;
+            };
+            files.my-secret-file = {
+              secret = true;
+            };
+            script = "echo test > $out/my-public-file";
+          };
+        };
+      };
+
+      filteredConfig = eval.config.clanInternals.machinesFiltered.x86_64-linux.test-machine.config;
+      unfilteredConfig = eval.config.clanInternals.machines.x86_64-linux.test-machine.config;
+    in
+    {
+      expr = {
+        # Both filtered and unfiltered should see the public var file
+        filteredValue = filteredConfig.clan.core.vars.generators.my-gen.files.my-public-file.value;
+        unfilteredValue = unfilteredConfig.clan.core.vars.generators.my-gen.files.my-public-file.value;
+        filteredExists = filteredConfig.clan.core.vars.generators.my-gen.files.my-public-file.exists;
+        unfilteredExists = unfilteredConfig.clan.core.vars.generators.my-gen.files.my-public-file.exists;
+
+      };
+      expected = {
+        filteredValue = "hello-from-var";
+        unfilteredValue = "hello-from-var";
+        filteredExists = true;
+        unfilteredExists = true;
+      };
+    };
+
+  machines_filtered_readfile_public_vars =
+    let
+      eval = clan {
+        self = {
+          inputs = { };
+        };
+        directory = ./cycle-test-flake;
+        meta.name = "test";
+
+        machines.test-machine = {
+          nixpkgs.hostPlatform = "x86_64-linux";
+
+          clan.core.vars.settings.publicStore = "in_repo";
+
+          clan.core.vars.generators.my-gen = {
+            files.my-public-file = {
+              secret = false;
+            };
+            files.my-secret-file = {
+              secret = true;
+            };
+            script = "echo test > $out/my-public-file";
+          };
+        };
+      };
+
+      filteredConfig = eval.config.clanInternals.machinesFiltered.x86_64-linux.test-machine.config;
+      unfilteredConfig = eval.config.clanInternals.machines.x86_64-linux.test-machine.config;
+    in
+    {
+      test_config_access = {
+        expr = builtins.readFile (
+          unfilteredConfig.clan.core.settings.directory
+          + "/vars/per-machine/test-machine/my-gen/my-public-file/value"
+        );
+        expected = "hello-from-var";
+      };
+      test_readfile_access = {
+        # Cannot open the file
+        expr = builtins.readFile (
+          filteredConfig.clan.core.settings.directory
+          + "/vars/per-machine/test-machine/my-gen/my-public-file/value"
+        );
+        expectedError = {
+          msg = "opening file";
+        };
+      };
+      # Test that secrets exist
+      test_exist_secret = {
+        expr = {
+          normal = builtins.pathExists (
+            unfilteredConfig.clan.core.settings.directory
+            + "/vars/per-machine/test-machine/my-gen/my-secret-file"
+          );
+          filtered = builtins.pathExists (
+            filteredConfig.clan.core.settings.directory + "/vars/per-machine/test-machine/my-gen/my-secret-file"
+          );
+        };
+        expected = {
+          normal = true;
+          filtered = false;
+        };
+      };
+    };
+
   test_clan_darwin_wireguard_service =
     let
       eval = clan {

modules/clan/module.nix
@@ -123,7 +123,13 @@ let
       _name: machine:
       machine.extendModules {
         modules = [
-          { clan.core.settings.directory = lib.mkForce filteredDirectory; }
+          {
+            clan.core.settings.directory = lib.mkForce filteredDirectory;
+            # Keep the unfiltered directory for reading committed var files.
+            # The filter on `directory` only prevents readDir cycles in generators;
+            # direct file reads of known var paths must still work.
+            clan.core.settings.varsDirectory = lib.mkForce directory;
+          }
           (lib.modules.importApply ../../nixosModules/machineModules/overridePkgs.nix {
             pkgs = pkgsFor.${system};
           })

nixosModules/clanCore/settings.nix
@@ -18,6 +18,18 @@ in
               the location of the flake repo, used to calculate the location of vars and secrets
             '';
           };
+          varsDirectory = mkOption {
+            internal = true;
+            visible = false;
+            type = types.path;
+            description = ''
+              the location used to read public var files (e.g. vars/per-machine/...).
+
+              Defaults to `directory`. This exists so that `machinesFiltered` can point
+              `directory` at a vars-free copy (to prevent readDir cycles in generators)
+              while still allowing direct file reads of committed var values.
+            '';
+          };
           name = lib.mkOption {
             type = lib.types.str;
             description = ''

nixosModules/clanCore/vars/public/in_repo.nix
@@ -14,12 +14,12 @@ let
           flakePath = mkIf (file.config.secret == false) (
             if file.config.share then
               (
-                config.clan.core.settings.directory
+                config.clan.core.settings.varsDirectory
                 + "/vars/shared/${file.config.generatorName}/${file.config.name}/value"
               )
             else
               (
-                config.clan.core.settings.directory
+                config.clan.core.settings.varsDirectory
                 + "/vars/per-machine/${config.clan.core.settings.machine.name}/${file.config.generatorName}/${file.config.name}/value"
               )
           );

nixosModules/clanCore/vars/secret/sops/default.nix
@@ -42,7 +42,7 @@ in
     #
     secrets = mapGeneratorsToSopsSecrets {
       inherit machineName;
-      directory = config.clan.core.settings.directory;
+      directory = config.clan.core.settings.varsDirectory;
       class = _class;
       generators = config.clan.core.vars.generators;
     };

nixosModules/machineModules/forName.nix
@@ -23,6 +23,7 @@
   clan.core.settings = {
     inherit (meta) name icon domain;
     inherit directory;
+    varsDirectory = directory;
     machine = {
       inherit name;
     };