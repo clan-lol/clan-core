7685bb8f396134df56ca8d7aa647ff7c12cc498c
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-03-13 17:38:25 +0100

    docs: disable devShell on x86_64-darwin for now

--- docs/nix/flake-module.nix
+++ docs/nix/flake-module.nix
@@ -8,36 +8,40 @@
       config,
       ...
     }:
-    let
-      clan-site-pkg = config.packages.clan-site;
-    in
     {
-      devShells.docs = pkgs.mkShellNoCC {
-        inputsFrom = [
-          clan-site-pkg
-        ];
-        packages = [
-          config.packages.clan-site-cli
-        ];
-        env = clan-site-pkg.devShellEnv;
-        shellHook = ''
-          export PRJ_ROOT=$(git rev-parse --show-toplevel)
+      devShells = lib.optionalAttrs (config.packages ? clan-site) (
+        let
+          clan-site-pkg = config.packages.clan-site;
+        in
+        {
+          docs = pkgs.mkShellNoCC {
+            inputsFrom = [
+              clan-site-pkg
+            ];
+            packages = [
+              config.packages.clan-site-cli
+            ];
+            env = clan-site-pkg.devShellEnv;
+            shellHook = ''
+              export PRJ_ROOT=$(git rev-parse --show-toplevel)
 
-          # Generate reference docs into the docs/site tree
-          mkdir -p $PRJ_ROOT/docs/site/reference/cli
-          cp -af ${self'.packages.module-docs}/services/* $PRJ_ROOT/docs/site/services/
-          cp -af ${self'.packages.module-docs}/reference/* $PRJ_ROOT/docs/site/reference/
-          cp -af ${self'.packages.clan-cli-docs}/* $PRJ_ROOT/docs/site/reference/cli/
-          chmod -R +w $PRJ_ROOT/docs/site
+              # Generate reference docs into the docs/site tree
+              mkdir -p $PRJ_ROOT/docs/site/reference/cli
+              cp -af ${self'.packages.module-docs}/services/* $PRJ_ROOT/docs/site/services/
+              cp -af ${self'.packages.module-docs}/reference/* $PRJ_ROOT/docs/site/reference/
+              cp -af ${self'.packages.clan-cli-docs}/* $PRJ_ROOT/docs/site/reference/cli/
+              chmod -R +w $PRJ_ROOT/docs/site
 
-          # Set up clan-site working directory
-          export CLAN_SITE_DIR=$PRJ_ROOT/pkgs/clan-site
-          cd $CLAN_SITE_DIR
-          ${clan-site-pkg.preBuild}
+              # Set up clan-site working directory
+              export CLAN_SITE_DIR=$PRJ_ROOT/pkgs/clan-site
+              cd $CLAN_SITE_DIR
+              ${clan-site-pkg.preBuild}
 
-          echo "Run 'clan-site' to start the live docs server"
-        '';
-      };
+              echo "Run 'clan-site' to start the live docs server"
+            '';
+          };
+        }
+      );
       packages = {
         docs-source = pkgs.callPackage ./docs-source.nix {
           inherit (self'.packages) module-docs clan-cli-docs;
