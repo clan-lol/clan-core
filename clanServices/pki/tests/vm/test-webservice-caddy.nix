eaa125cb42ec1d80472d3e42afbd5e37f970ce0f
a-kenji <aks.kenji@protonmail.com>
2026-03-17 15:42:14 +0100

    eval/warnings: Address eval warnings
    Replace deprecated xorg.lndir with lndir and add dummy
    description/readme fields to test-only services that were
    missing them.

--- clanServices/pki/tests/vm/test-webservice-caddy.nix
+++ clanServices/pki/tests/vm/test-webservice-caddy.nix
@@ -3,9 +3,12 @@
   _class = "clan.service";
   manifest.name = "test-webservice-caddy";
   manifest.description = "Dummy caddy web service for pki testing";
+  manifest.readme = "Dummy readme to silence eval warnings.";
   manifest.exports.out = [ "endpoints" ];
 
   roles.default = {
+    description = "Dummy description to silence eval warnings.";
+
     perInstance =
       { machine, mkExports, ... }:
       let
