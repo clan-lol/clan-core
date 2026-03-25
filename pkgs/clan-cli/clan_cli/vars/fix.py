ec167f528e58153e0fd477d5101696d64c5cc796
a-kenji <aks.kenji@protonmail.com>
2026-03-25 11:20:19 +0100

    pkgs/clan/cli: Fix whole vars by default
    `clan vars fix` without `[MACHINES..]` specified will now fix
    vars by default for the whole clan, bringing it in line with other clan
    commands.
    
    Closes: #7150

--- pkgs/clan-cli/clan_cli/vars/fix.py
+++ pkgs/clan-cli/clan_cli/vars/fix.py
@@ -2,29 +2,39 @@ import argparse
 
 from clan_cli.completions import add_dynamic_completer, complete_machines
 from clan_lib.flake import require_flake
-from clan_lib.machines.machines import Machine
+from clan_lib.machines.list import list_full_machines
 from clan_lib.vars.fix import fix_vars
 
 
 def fix_command(args: argparse.Namespace) -> None:
     flake = require_flake(args.flake)
-    machine = Machine(
-        name=args.machine,
-        flake=flake,
-    )
-    fix_vars(machine, generator_name=args.generator)
+    machines = list(list_full_machines(flake).values())
+
+    if len(args.machines) > 0:
+        machines = list(
+            filter(
+                lambda m: m.name in args.machines,
+                machines,
+            ),
+        )
+
+    for machine in machines:
+        fix_vars(machine, generator_name=args.generator)
 
 
 def register_fix_parser(parser: argparse.ArgumentParser) -> None:
     machines_parser = parser.add_argument(
-        "machine",
-        help="The machine to fix vars for",
+        "machines",
+        type=str,
+        help="machine to fix vars for. if empty, fix vars for all machines",
+        nargs="*",
+        default=[],
     )
     add_dynamic_completer(machines_parser, complete_machines)
 
     parser.add_argument(
         "--generator",
         "-g",
-        help="the generator to check",
+        help="the generator to fix",
     )
     parser.set_defaults(func=fix_command)
