c05e959f917dcacbb01f07c8e67a4630443891d8
Glen Huang <me@glenhuang.com>
2026-04-15 21:59:35 +0800

    clan-site: update assets

--- pkgs/clan-site/nix/assets.nix
+++ pkgs/clan-site/nix/assets.nix
@@ -7,8 +7,8 @@ let
     sha256 = "sha256:1p6z8j2zp2i3ppm4f4lypy9gdw4abkc5bsnafdy14b7wr78sbrck";
   };
   assets = fetchTarball {
-    url = "https://git.clan.lol/clan/assets/archive/eb81db9ca168593d203b62e33914ffb904d59689.tar.gz";
-    sha256 = "sha256:1b4zfblcdrbpdhljyp4xp24sk9nn4jj7lfiyxafpca86dafnvadm";
+    url = "https://git.clan.lol/clan/assets/archive/1347b6d6ec02fbc94e99cbb2acc8a05ec52a26ba.tar.gz";
+    sha256 = "sha256:0gk0d81d7fsghdhig5whxx7ymdn5cjc73jw7vfyhird4sq964wix";
   };
 in
 runCommand "clan-site-assets" { } ''
