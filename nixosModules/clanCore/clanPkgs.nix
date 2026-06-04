68675cc38b35b9505c4133f4ec70af2af43569ee
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-06-04 11:42:56 +0200

    clanPkgs: add deprecation warning

--- nixosModules/clanCore/clanPkgs.nix
+++ nixosModules/clanCore/clanPkgs.nix
@@ -1,9 +1,17 @@
 { lib, pkgs, ... }:
 {
-  # TODO: Move this into settings.clanPkgs ?
-  # This could also be part of the public interface to allow users to override the internal packages
   options.clan.core.clanPkgs = lib.mkOption {
     defaultText = "self.packages.${pkgs.stdenv.hostPlatform.system}";
     internal = true;
+    apply =
+      v:
+      lib.warn ''
+        'clan.core.clanPkgs' is deprecated and will be removed in the next release.
+
+        If you reference 'clanPkgs.zerotier-members' or 'clanPkgs.zerotierone'
+        See the [zerotier-migration guide](https://clan.lol/docs/unstable/guides/migrations/zerotier)
+
+        If you reference any other attribute contact us on matrix: https://matrix.to/#/#clan:clan.lol
+      '' v;
   };
 }
