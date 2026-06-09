a601d5fafa4b08a101f1a5ea4623c544e2877343
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-06-09 19:18:13 +0200

    devFlake: drop nuschtos

--- devFlake/flake.nix
+++ devFlake/flake.nix
@@ -7,9 +7,6 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-utils.inputs.systems.follows = "systems";
 
-  inputs.nuschtos.url = "github:NuschtOS/search";
-  inputs.nuschtos.inputs.nixpkgs.follows = "nixpkgs-dev";
-
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
   inputs.treefmt-nix.inputs.nixpkgs.follows = "";
 
